256caae146c203b8ad89b9c793c8335674ff044f
Daniel Billing <daniel@retardedtech.com>
2016-02-03 21:51:08 +0100

    LANMS-5 PDF Ticket shows if paid or not

--- resources/views/neon-user/seating/pdf/ticket.blade.php
+++ resources/views/neon-user/seating/pdf/ticket.blade.php
@@ -3,17 +3,19 @@
 		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 		<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
 		<style type="text/css">
-			body {font-family:'Open Sans',sans-serif;overflow:auto;margin:0;font-size:14px;line-height:1.25;color:#333;}
+			body {font-family:'Open Sans',sans-serif;overflow:auto;margin:0;font-size:14px;line-height:1.25;color:#333}
 			.row {overflow:auto}
-			.col-md-6 {width:50%;float:left;display: inline-block;}
-			.col-md-12 {width:100%;float:left;display:inline-block;}
+			.col-md-6 {width:50%;float:left;display:inline-block}
+			.col-md-12 {width:100%;float:left;display:inline-block}
 			.text-center {text-align:center}
-			.text-muted {color:#999;}
-			small {font-size:95%;color:#999;}
-			h1 {font-size:3em;margin:0;padding:0;}
-			h2 {font-size:2em;margin:0;padding:0;}
-			p {margin:0;padding:0;}
-			hr {margin-top: 17px;margin-bottom: 17px;border: 0;border-top: 1px solid #eeeeee;}
+			.text-muted {color:#999}
+			.text-danger {color: #ac1818}
+			.text-success {color: #045702}
+			small {font-size:95%;color:#999}
+			h1 {font-size:3em;margin:0;padding:0}
+			h2 {font-size:2em;margin:0;padding:0}
+			p {margin:0;padding:0}
+			hr {margin-top: 17px;margin-bottom: 17px;border: 0;border-top: 1px solid #eee}
 		</style>
 	</head>
 	<body>	
@@ -27,8 +29,18 @@
 			<br>
 			<p>For more information, see our website: <strong>{{ Setting::get('WEB_PROTOCOL') }}://{{ Setting::get('WEB_DOMAIN') }}@if(Setting::get('WEB_PORT') <> 80){{ ':'.Setting::get('WEB_PORT') }}@endif/</strong></p>
 			<br>
-			<h2><strong><small>Your seat:</small><br>{{ $seat->reservations->first()->seat->name }}</strong></h2>
-			<br><br>
+			<div class="row">
+				<div class="col-md-6">
+					@if(is_null($seat->reservations->first()->payment))
+						<h2><strong><small>Paid:</small><br><span class="text-danger">No</span></strong></h2>
+					@else
+						<h2><strong><small>Paid:</small><br><span class="text-success">Yes</span></strong></h2>
+					@endif
+				</div>
+				<div class="col-md-6">
+					<h2><strong><small>Your seat:</small><br>{{ $seat->reservations->first()->seat->name }}</strong></h2>
+				</div>
+			</div>
 			<hr>
 			<br><br>
 		</div>
