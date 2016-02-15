4f837bfa551558088ad35dc3badb201de9756cb9
Daniel Billing <daniel@retardedtech.com>
2016-02-15 23:05:49 +0100

    LANMS-59

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -14,16 +14,21 @@
 				<div class="row post">
 					<div class="col s12">
 						<div class="card">
+							<div class="card-image">
+								<h2 class="card-title"><a href="{{ route('news-show', $article->slug) }}">{{ $article->title }}</a></h2>
+							</div>
 							<div class="card-content">
-								<a href="{{ route('news-show', $article->slug) }}"><h1>{{ $article->title }}</h1></a>
+								<p>
+									<small>Published: {{ date(User::getUserDateFormat(), strtotime($article->published_at)) .' at '. date(User::getUserTimeFormat(), strtotime($article->published_at)) }} by <a class="author" href="{{ URL::route('user-profile', $article->author->username) }}">{{ User::getFullnameByID($article->author->id) }}</a> &middot; Updated: {{ date(User::getUserDateFormat(), strtotime($article->updated_at))  .' at '. date(User::getUserTimeFormat(), strtotime($article->updated_at)) }} by <a href="{{ URL::route('user-profile', $article->editor->username) }}">{{ User::getFullnameByID($article->editor->id) }}</a></small>
+								</p>
+								<hr>
 								<p>{!! substr($article->content, 0, 1000) !!}@if(strlen($article->content) >= 1000)...@endif</p>
 							</div>
-							<div class="card-action">
-								<small>Published: {{ date(User::getUserDateFormat(), strtotime($article->published_at)) .' at '. date(User::getUserTimeFormat(), strtotime($article->published_at)) }} by <a class="author" href="{{ URL::route('user-profile', $article->author->username) }}">{{ User::getFullnameByID($article->author->id) }}</a> &middot; Updated: {{ date(User::getUserDateFormat(), strtotime($article->updated_at))  .' at '. date(User::getUserTimeFormat(), strtotime($article->updated_at)) }} by <a href="{{ URL::route('user-profile', $article->editor->username) }}">{{ User::getFullnameByID($article->editor->id) }}</a></small>
 							@if(strlen($article->content) >= 1000)
-								<a href="{{ URL::route('news-show', $article->slug) }}" class="btn btn-primary btn-xs pull-right newsBtn"><i class="fa fa-arrow-circle-right"></i> Les mer</a>
+								<div class="card-action">
+									<a href="{{ URL::route('news-show', $article->slug) }}" class="btn btn-primary btn-xs pull-right newsBtn"><i class="fa fa-arrow-circle-right"></i> Les mer</a>
+								</div>
 							@endif
-							</div>
 						</div>
 					</div>
 				</div>
