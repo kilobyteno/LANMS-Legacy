31e7beb38cb4ae90868193f678e0dba947309fee
Daniel Billing <daniel@infihex.com>
2017-01-15 13:18:16 +0100

    Updated to new relations and scopes

--- resources/views/neon-user/seating/seatmap.blade.php
+++ resources/views/neon-user/seating/seatmap.blade.php
@@ -11,16 +11,17 @@
 						<li class="seat kiosk" id="kiosk"><p><span class="fa fa-coffee"></span></p></li>
 					@endif
 					@foreach($row->seats as $seat)
-						<li class="seat @if($seat->reservation->thisyear()->count() > 0) @if($seat->reservation->thisyear()->status->id == 1) seat-reserved @elseif($seat->reservation->thisyear()->status->id == 2) seat-tempreserved @endif @if(Sentinel::getUser()->id == $seat->reservation->thisyear()->first()->reservedfor->id and $seat->reservation->thisyear()->first()->status->id == 1) seat-yours @endif  @endif @if(Request::segment(3) == $seat->slug) active @endif ">
+					
+						<li class="seat @if($seat->reservationsThisYear()->count() > 0) @if($seat->reservationsThisYear()->first()->status->id == 1) seat-reserved @elseif($seat->reservationsThisYear()->first()->status->id == 2) seat-tempreserved @endif @if(Sentinel::getUser()->id == $seat->reservationsThisYear()->first()->reservedfor->id and $seat->reservationsThisYear()->first()->status->id == 1) seat-yours @endif  @endif @if(Request::segment(3) == $seat->slug) active @endif ">
 							<p>
-								@if($seat->reservation->thisyear()->count() == 0)
+								@if($seat->reservationsThisYear()->count() == 0)
 									<a href="{{ URL::route('seating-show', $seat->slug) }}" data-toggle="tooltip" title="Available">{{ $seat->name }}</a>
-								@elseif(Sentinel::getUser()->id == $seat->reservation->thisyear()->reservedfor->id and $seat->reservation->thisyear()->status->id == 1)
+								@elseif(Sentinel::getUser()->id == $seat->reservationsThisYear()->first()->reservedfor->id and $seat->reservationsThisYear()->first()->status->id == 1)
 									<a href="{{ URL::route('seating-show', $seat->slug) }}" data-toggle="tooltip" title="This seat is reserved for you!">{{ $seat->name }}</a>
-								@elseif($seat->reservation->thisyear()->status->id == 1)
-									<a href="{{ URL::route('seating-show', $seat->slug) }}" data-toggle="tooltip" title="Reserved for: {{ User::getUsernameAndFullnameByID($seat->reservation->thisyear()->reservedfor->id) }}">{{ $seat->name }}</a>
-								@elseif($seat->reservation->thisyear()->status->id == 2)
-									<a href="{{ URL::route('seating-show', $seat->slug) }}" data-toggle="tooltip" title="Temporary Reserved By: {{ User::getUsernameAndFullnameByID($seat->reservation->thisyear()->reservedfor->id) }}">{{ $seat->name }}</a>
+								@elseif($seat->reservationsThisYear()->first()->status->id == 1)
+									<a href="{{ URL::route('seating-show', $seat->slug) }}" data-toggle="tooltip" title="Reserved for: {{ User::getUsernameAndFullnameByID($seat->reservationsThisYear()->first()->reservedfor->id) }}">{{ $seat->name }}</a>
+								@elseif($seat->reservationsThisYear()->first()->status->id == 2)
+									<a href="{{ URL::route('seating-show', $seat->slug) }}" data-toggle="tooltip" title="Temporary Reserved By: {{ User::getUsernameAndFullnameByID($seat->reservationsThisYear()->first()->reservedfor->id) }}">{{ $seat->name }}</a>
 								@else
 									{{ $seat->name }}
 								@endif
