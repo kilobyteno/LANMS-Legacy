f0a7a7f8353a8cfd99e0dc3f0b2f05c16837f4b3
Daniel Billing <daniel@infihex.com>
2017-01-15 22:49:02 +0100

    LANMS-91

--- resources/views/neon-admin/home.blade.php
+++ resources/views/neon-admin/home.blade.php
@@ -12,7 +12,40 @@
 					
 		<br />
 
-		<p>Admin Dashboard!</p>
+		<div class="row">
+			<div class="col-sm-3">
+				<a class="tile-stats tile-aqua">
+					<div class="icon"><i class="fa fa-users"></i></div>
+					<div class="num" data-start="0" data-end="{{User::all()->count()}}" data-duration="1500" data-delay="0">{{User::all()->count()}}</div>
+					<h3>Registered users</h3>
+					<p>so far on our website.</p>
+				</a>
+			</div>
+			<div class="col-sm-3">
+				<a class="tile-stats tile-cyan" href="{{route('admin-seating')}}">
+					<div class="icon"><i class="fa fa-street-view"></i></div>
+					<div class="num" data-start="0" data-end="{{SeatReservation::thisYear()->count()}}" data-duration="1500" data-delay="0">{{SeatReservation::thisYear()->count()}}</div>
+					<h3>Seat reservations</h3>
+					<p>so far this year.</p>
+				</a>
+			</div>
+			<div class="col-sm-3">
+				<a class="tile-stats tile-gray" href="{{route('admin-news')}}">
+					<div class="icon"><i class="fa fa-newspaper-o"></i></div>
+					<div class="num" data-start="0" data-end="{{News::isPublished()->count()}}" data-duration="1500" data-delay="0">{{News::isPublished()->count()}}</div>
+					<h3>News articles</h3>
+					<p>published.</p>
+				</a>
+			</div>
+			<div class="col-sm-3">
+				<a class="tile-stats tile-primary" href="{{route('admin-pages')}}">
+					<div class="icon"><i class="fa fa-file-text"></i></div>
+					<div class="num" data-start="0" data-end="{{Page::all()->count()}}" data-duration="1500" data-delay="0">{{Page::all()->count()}}</div>
+					<h3>Pages</h3>
+					<p>created and shown on the website.</p>
+				</a>
+			</div>
+		</div>
 	</div>
 </div>
 @stop
