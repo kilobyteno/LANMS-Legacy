13befaa361ae0bb292eee9effc526f95dd3b6ad2
Daniel Billing <daniel@infihex.com>
2017-01-15 13:18:52 +0100

    Oh damn, forgot this too. Updated to new relations and scopes

--- resources/views/neon-user/seating/show.blade.php
+++ resources/views/neon-user/seating/show.blade.php
@@ -34,39 +34,39 @@
 
 				</div>
 				<div class="col-md-8">
-					@if($currentseat->reservation)
+					@if($currentseat->reservationsThisYear()->first())
 						<div class="alert alert-info" role="alert">
-							<strong>Information:</strong> This seat is {{ strtolower($currentseat->reservation->status->name) }}.
+							<strong>Information:</strong> This seat is {{ strtolower($currentseat->reservationsThisYear()->first()->status->name) }}.
 						</div>
 						<div class="member-entry">
-							<a href="{{ route('user-profile', $currentseat->reservation->reservedfor->username) }}" class="member-img">
-								<img src="{{ $currentseat->reservation->reservedfor->profilepicture or '/images/profilepicture/0.png' }}" class="img-rounded" />
+							<a href="{{ route('user-profile', $currentseat->reservationsThisYear()->first()->reservedfor->username) }}" class="member-img">
+								<img src="{{ $currentseat->reservationsThisYear()->first()->reservedfor->profilepicture or '/images/profilepicture/0.png' }}" class="img-rounded" />
 								<i class="fa fa-share" style="text-shadow:#000 0 0 10px"></i>
 							</a>
 							<div class="member-details">
 								<h4>
-									<a href="{{ route('user-profile', $currentseat->reservation->reservedfor->username) }}">{{ $currentseat->reservation->reservedfor->firstname }}@if($currentseat->reservation->reservedfor->showname) {{ $currentseat->reservation->reservedfor->lastname }}@endif</a>
+									<a href="{{ route('user-profile', $currentseat->reservationsThisYear()->first()->reservedfor->username) }}">{{ $currentseat->reservationsThisYear()->first()->reservedfor->firstname }}@if($currentseat->reservationsThisYear()->first()->reservedfor->showname) {{ $currentseat->reservationsThisYear()->first()->reservedfor->lastname }}@endif</a>
 								</h4>
 								<div class="row info-list">
-									@if($currentseat->reservation->reservedfor->occupation)
+									@if($currentseat->reservationsThisYear()->first()->reservedfor->occupation)
 										<div class="col-sm-6">
-											<i class="fa fa-briefcase"></i> {{ $currentseat->reservation->reservedfor->occupation }}
+											<i class="fa fa-briefcase"></i> {{ $currentseat->reservationsThisYear()->first()->reservedfor->occupation }}
 										</div>
 									@endif
-									@if($currentseat->reservation->reservedfor->location)
+									@if($currentseat->reservationsThisYear()->first()->reservedfor->location)
 										<div class="col-sm-6">
-											<i class="fa fa-map-marker"></i> {{ $currentseat->reservation->reservedfor->location or '<em>Unkown</em>' }}
+											<i class="fa fa-map-marker"></i> {{ $currentseat->reservationsThisYear()->first()->reservedfor->location or '<em>Unkown</em>' }}
 										</div>
 									@endif
 									<div class="clear"></div>
-									@if($currentseat->reservation->reservedfor->gender)
+									@if($currentseat->reservationsThisYear()->first()->reservedfor->gender)
 										<div class="col-sm-6">
-											<i class="fa fa-genderless"></i> {{ $currentseat->reservation->reservedfor->gender }}
+											<i class="fa fa-genderless"></i> {{ $currentseat->reservationsThisYear()->first()->reservedfor->gender }}
 										</div>
 									@endif
-									@if($currentseat->reservation->reservedfor->birthdate)
+									@if($currentseat->reservationsThisYear()->first()->reservedfor->birthdate)
 										<div class="col-sm-6">
-											<i class="fa fa-birthday-cake"></i> {{ date_diff(date_create($currentseat->reservation->reservedfor->birthdate), date_create('today'))->y }}
+											<i class="fa fa-birthday-cake"></i> {{ date_diff(date_create($currentseat->reservationsThisYear()->first()->reservedfor->birthdate), date_create('today'))->y }}
 										</div>
 									@endif
 								</div>
@@ -76,7 +76,7 @@
 						<div class="alert alert-info" role="alert">
 							<strong>Information:</strong> This seat cannot be reserved!
 						</div>
-					@elseif(Sentinel::getUser()->reservations->count() >= 5)
+					@elseif(Sentinel::getUser()->reservationsThisYear()->count() >= 5)
 						<div class="alert alert-warning" role="alert">
 							<strong>Warning!</strong> You are not allowed to reserve more than <em>five</em> seats.
 						</div>
@@ -93,7 +93,7 @@
 										<p class="text-danger">{{ $errors->first('reservedfor') }}</p>
 									@endif
 									<div class="checkbox">
-										<label><input type="checkbox" id="tos"> I have read, accepted and agreed to the <a href="{{ route('tos') }}" target="_blank">Terms of Service</a>.</label>
+										<label><input type="checkbox" id="tos"> I have read, accepted and agreed to the <a href="{{ url('tos') }}" target="_blank">Terms of Service</a>.</label>
 									</div>
 								</div>
 							</div>
