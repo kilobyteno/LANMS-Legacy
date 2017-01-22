9fbfa12c5db233f4be04e769d65a4e50805face9
Daniel Billing <daniel@infihex.com>
2017-01-22 20:58:47 +0100

    Updated models according to DB

--- app/SeatRows.php
+++ app/SeatRows.php
@@ -1,9 +1,13 @@
 <?php namespace LANMS;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SeatRows extends Model {
 
+	use SoftDeletes;
+
+	protected $dates = ['deleted_at'];
 	protected $table = 'seat_rows';
 
 	public function seats() {
--- app/Seats.php
+++ app/Seats.php
@@ -1,9 +1,13 @@
 <?php namespace LANMS;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Seats extends Model {
 
+	use SoftDeletes;
+
+	protected $dates = ['deleted_at'];
 	protected $table = 'seats';
 
 	protected $fillable = [
