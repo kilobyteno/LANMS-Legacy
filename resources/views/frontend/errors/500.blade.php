056f986767afd69398070706ef0fc2bc1490e732
Daniel Billing <daniel@infihex.com>
2017-01-22 20:59:47 +0100

    Updated 500 page to not include org theme if it fails to load pages

--- resources/views/frontend/errors/500.blade.php
+++ resources/views/frontend/errors/500.blade.php
@@ -1,15 +1,52 @@
-@extends('layouts.main')
-@section('title', '500')
-   
-@section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-6 col-md-offset-3 text-center">
-			<h1 class="error-header">500</h1>
-			<h3 class="error-lead text-muted">Oops! We track these errors automatically, but if the problem persists feel free to contact us. In the meantime, try refreshing.</h3>
+<html>
+	<head>
+		<link href='//fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
+		<title>500</title>
+		<style>
+			body {
+				margin: 0;
+				padding: 0;
+				width: 100%;
+				height: 100%;
+				color: #555;
+				display: table;
+				font-weight: 300;
+				font-family: 'Lato';
+			}
+
+			.container {
+				text-align: center;
+				display: table-cell;
+				vertical-align: middle;
+			}
+
+			.content {
+				text-align: center;
+				display: inline-block;
+			}
+
+			.title {
+				font-size: 72px;
+				margin-bottom: 40px;
+			}
+
+			.hero {
+				font-size: 20px;
+			}
+			small {
+				font-size:75%;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<div class="content">
+				<div class="title">500</div>
+				<div class="hero">Oops! We track these errors automatically, but if the problem persists feel free to contact us. In the meantime, try refreshing.</div>
+			</div>
 		</div>
-	</div>
-</div>
+	</body>
+</html>
 @unless(empty($sentryID))
 	<!-- Sentry JS SDK 2.1.+ required -->
 	<script src="https://cdn.ravenjs.com/3.3.0/raven.min.js"></script>
@@ -23,4 +60,3 @@
 	});
 	</script>
 @endunless
-@stop
