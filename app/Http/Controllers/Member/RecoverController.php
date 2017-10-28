af64e72554828aff7ad097c03738aaeac9ce83ca
Daniel Billing <daniel@infihex.com>
2017-10-29 01:20:49 +0200

    Removed deprecated code and a comment

--- app/Http/Controllers/Member/RecoverController.php
+++ app/Http/Controllers/Member/RecoverController.php
@@ -15,34 +15,7 @@ use LANMS\Rem;
 
 class RecoverController extends Controller {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Password Reset Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller is responsible for handling password reset requests
-	| and uses a simple trait to include this behavior. You're free to
-	| explore this trait and override any methods you wish to tweak.
-	|
-	*/
-
 	use ResetsPasswords;
-
-	/**
-	 * Create a new password controller instance.
-	 *
-	 * @param  \Illuminate\Contracts\Auth\Guard  $auth
-	 * @param  \Illuminate\Contracts\Auth\PasswordBroker  $passwords
-	 * @return void
-	 */
-	public function __construct(Guard $auth, PasswordBroker $passwords)
-	{
-		$this->auth = $auth;
-		$this->passwords = $passwords;
-
-		$this->beforeFilter('csrf', ['on' => ['post']]);
-		$this->middleware('sentinel.guest');
-	}
 
 	public function getForgotPassword() {
 		return view('auth.forgot-password');
