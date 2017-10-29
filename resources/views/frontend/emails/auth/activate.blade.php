cb4da20f7de67d18e991359eecb2bfde49c2765e
Daniel Billing <daniel@infihex.com>
2017-10-29 10:53:50 +0100

    Removed from frontend folder

--- resources/views/frontend/emails/auth/activate.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('emails.layouts.main')
-@section('subject', 'Activate Account')
-@section('content') 
-
-<p>Hello {{ $firstname }}! To activate your account, click on the following link:</p>
-<p><a href="{{ $link }}">{{ $link }}</a></p>
-
-@stop
