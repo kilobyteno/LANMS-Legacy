1f9499a6a6682a5bad83ac873daa6f702b4dd66e
Daniel Billing <daniel@infihex.com>
2017-11-11 10:40:52 +0100

    Seatmap update

--- resources/views/neon-user/seating/seatmap.blade.php
+++ resources/views/neon-user/seating/seatmap.blade.php
@@ -1,17 +1,14 @@
 <div class="seatmap">
 	<ul>
-		<li class="scene">Scene</li>
 		<li class="entrance" id="entrance"><p><span class="fa fa-sign-in"></span></p></li>
-
 		@foreach($rows as $row)
-
 			<li class="seat-row">
 				<ul class="seat-row-{{$row->slug}}">
-					@if($row->slug == 'a')
-						<li class="seat kiosk" id="kiosk"><p><span class="fa fa-coffee"></span></p></li>
-					@endif
-					@foreach($row->seats as $seat)
 					
+					@foreach($row->seats as $seat)
+						@if($seat->slug == 'a6')
+							<li class="seat kiosk" id="kiosk"><p><span class="fa fa-coffee"></span></p></li>
+						@endif
 						<li class="seat @if($seat->reservationsThisYear()->count() > 0) @if($seat->reservationsThisYear()->first()->status->id == 1) seat-reserved @elseif($seat->reservationsThisYear()->first()->status->id == 2) seat-tempreserved @endif @if(Sentinel::getUser()->id == $seat->reservationsThisYear()->first()->reservedfor->id and $seat->reservationsThisYear()->first()->status->id == 1) seat-yours @endif  @endif @if(Request::segment(3) == $seat->slug) active @endif ">
 							<p>
 								@if($seat->reservationsThisYear()->count() == 0)
