d69e5d1d4de84b8172088e6b9a5d954f3cf461a2
Daniel Billing <daniel@infihex.com>
2017-11-13 19:26:48 +0100

    Added db changes to seeder LANMS-80

--- database/seeds/CrewTableSeeder.php
+++ database/seeds/CrewTableSeeder.php
@@ -13,26 +13,39 @@ class CrewTableSeeder extends Seeder  {
 		Crew::create([
 			'user_id' 		=> 1,
 			'category_id' 	=> 1,
+			'author_id' 	=> 1,
+			'editor_id' 	=> 1,
+			'year'			=> 2018,
 		]);
 
 		Crew::create([
 			'user_id' 		=> 2,
 			'category_id' 	=> 2,
+			'author_id' 	=> 1,
+			'editor_id' 	=> 1,
+			'year'			=> 2018,
 		]);
 
 		Crew::create([
 			'user_id' 		=> 3,
 			'category_id' 	=> 2,
+			'author_id' 	=> 1,
+			'editor_id' 	=> 1,
+			'year'			=> 2018,
 		]);
 
 		CrewCategory::create([
 			'title' 	=> 'Core',
 			'slug' 		=> 'core',
+			'author_id' => 1,
+			'editor_id' => 1,
 		]);
 
 		CrewCategory::create([
 			'title' 	=> 'Crew',
 			'slug' 		=> 'crew',
+			'author_id' => 1,
+			'editor_id' => 1,
 		]);
 
 		CrewSkill::create([
@@ -40,6 +53,8 @@ class CrewTableSeeder extends Seeder  {
 			'slug' 		=> 'admin',
 			'icon' 		=> 'fa fa-user-secret',
 			'label' 	=> 'label label-primary',
+			'author_id' => 1,
+			'editor_id' => 1,
 		]);
 
 		CrewSkill::create([
@@ -47,6 +62,8 @@ class CrewTableSeeder extends Seeder  {
 			'slug' 		=> 'network',
 			'icon' 		=> 'fa fa-wifi',
 			'label' 	=> 'label label-success',
+			'author_id' => 1,
+			'editor_id' => 1,
 		]);
 
 		CrewSkill::create([
@@ -54,36 +71,56 @@ class CrewTableSeeder extends Seeder  {
 			'slug' 		=> 'security',
 			'icon' 		=> 'fa fa-lock',
 			'label' 	=> 'label label-warning',
+			'author_id' => 1,
+			'editor_id' => 1,
 		]);
 
 		CrewSkillAttached::create([
 			'user_id' 	=> 1,
 			'skill_id' 	=> 1,
+			'author_id' => 1,
+			'editor_id' => 1,
+			'year'		=> 2018,
 		]);
 
 		CrewSkillAttached::create([
 			'user_id' 	=> 1,
 			'skill_id' 	=> 2,
+			'author_id' => 1,
+			'editor_id' => 1,
+			'year'		=> 2018,
 		]);
 
 		CrewSkillAttached::create([
 			'user_id' 	=> 1,
 			'skill_id' 	=> 3,
+			'author_id' => 1,
+			'editor_id' => 1,
+			'year'		=> 2018,
 		]);
 
 		CrewSkillAttached::create([
 			'user_id' 	=> 2,
 			'skill_id' 	=> 2,
+			'author_id' => 1,
+			'editor_id' => 1,
+			'year'		=> 2018,
 		]);
 
 		CrewSkillAttached::create([
 			'user_id' 	=> 2,
 			'skill_id' 	=> 3,
+			'author_id' => 1,
+			'editor_id' => 1,
+			'year'		=> 2018,
 		]);
 
 		CrewSkillAttached::create([
 			'user_id' 	=> 3,
 			'skill_id' 	=> 3,
+			'author_id' => 1,
+			'editor_id' => 1,
+			'year'		=> 2018,
 		]);
 
 	}
