8404f078574cafd7f1a8d14d2e9829c5475cf965
Daniel Billing <daniel@infihex.com>
2017-11-21 13:03:59 +0100

    Updated responsiveness and template for admin LANMS-61

--- resources/views/neon-admin/seating/seatmap.blade.php
+++ resources/views/neon-admin/seating/seatmap.blade.php
@@ -1,17 +1,25 @@
-<div class="seatmap">
-	<ul>
-		<li class="scene">Scene</li>
-		<li class="entrance" id="entrance"><p><span class="fa fa-sign-in"></span></p></li>
-
+<ul class="seatmap">
+	<li class="top-row">
+		<ul>
+			<li class="emergency-exit emergency-exit-top"><p>Nødutgang</p></li>
+			<li class="entrance"><p>INNGANG</p></li>
+		</ul>
+	</li>
+	<li class="emergency-row">
+		<ul>
+			<li class="emergency-exit emergency-exit-left"><p>Nødutgang</p></li>
+			<li class="emergency-exit emergency-exit-left gate"><p>Port</p></li>
+		</ul>
+	</li>
+	<ul class="seat-rows">
 		@foreach($rows as $row)
-
-			<li class="seat-row">
+			<li class="seat-row" @if($row->slug == 'l') style="margin-top:20px;" @endif>
 				<ul class="seat-row-{{$row->slug}}">
-					@if($row->slug == 'a')
-						<li class="seat kiosk" id="kiosk"><p><span class="fa fa-coffee"></span></p></li>
-					@endif
-					@foreach($row->seats as $seat)
 					
+					@foreach($row->seats as $seat)
+						@if($seat->slug == 'a7')
+							<li class="seat kiosk" id="kiosk"><p><span class="fa fa-coffee"></span></p></li>
+						@endif
 						<li class="seat @if($seat->reservationsThisYear()->count() > 0) @if($seat->reservationsThisYear()->first()->status->id == 1) seat-reserved @elseif($seat->reservationsThisYear()->first()->status->id == 2) seat-tempreserved @endif @if(Sentinel::getUser()->id == $seat->reservationsThisYear()->first()->reservedfor->id and $seat->reservationsThisYear()->first()->status->id == 1) seat-yours @endif  @endif @if(Request::segment(3) == $seat->slug) active @endif ">
 							<p>
 								@if($seat->reservationsThisYear()->count() == 0)
@@ -32,4 +40,10 @@
 			</li>
 		@endforeach
 	</ul>
-</div>
+	<li class="emergency-row-bottom">
+		<ul>
+			<li class="emergency-exit emergency-exit-bottom"><p>Nødutgang</p></li>
+			<li class="emergency-exit emergency-exit-bottom"><p>Nødutgang</p></li>
+		</ul>
+	</li>
+</ul>
