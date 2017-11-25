8821eed1d865809e8c076671abec099ae81ae01f
Daniel Billing <daniel@infihex.com>
2017-11-25 12:13:41 +0100

    Renamed "Admin Dashboard" to "Dashboard

--- resources/views/neon-admin/dashboard.blade.php
+++ resources/views/neon-admin/dashboard.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.main')
-@section('title', 'Admin Dashboard')
+@section('title', 'Dashboard - Admin')
    
 @section('content')
 <div class="row">
 	<div class="col-md-12">
-		<h1 class="margin-bottom">Admin Dashboard</small></h1>
+		<h1 class="margin-bottom">Dashboard</small></h1>
 		<ol class="breadcrumb 2" >
 			<li><a href="{{ route('home') }}"><i class="fa fa-home"></i>Home</a></li>
-			<li class="active"><strong>Admin Dashboard</strong></li>
+			<li>Admin</li>
+			<li class="active"><strong>Dashboard</strong></li>
 		</ol>
 					
 		<br />
