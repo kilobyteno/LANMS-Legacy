060f16fa629b888eca18c9fc7cfcf1e5e4e484c1
Daniel Billing <daniel@infihex.com>
2017-11-25 21:32:00 +0100

    LANMS-137

--- resources/views/neon-admin/seating/print/index.blade.php
+++ resources/views/neon-admin/seating/print/index.blade.php
@@ -1,24 +1,6 @@
 @extends('layouts.main')
 @section('title', 'Print - Admin')
 @section('css')
-	<style>
-		.ms-container .ms-list {
-			width: 135px;
-			height: 205px;
-		}
-		
-		.post-save-changes {
-			float: right;
-		}
-		
-		@media screen and (max-width: 789px)
-		{
-			.post-save-changes {
-				float: none;
-				margin-bottom: 20px;
-			}
-		}
-	</style>
 	<link rel="stylesheet" href="{{ Theme::url('css/seating.css') }}">
 @stop
 @section('content')
@@ -39,26 +21,4 @@
 	</div>
 </div>
 
-@stop
-
-@section('javascript')
-	<script src="{{ Theme::url('js/bootstrap-typeahead.min.js') }}"></script>
-	<script type="text/javascript">
-		(function($) {
-			$(document).ready( function() { 
-				$('#seat_name').typeahead({
-					onSelect: function(item) {
-						console.log(item.value);
-					},
-					ajax: {
-						url: "/ajax/seats",
-						timeout: 500,
-						displayField: "name",
-						triggerLength: 1,
-						method: "get",
-					}
-				});
-			 });
-		})(jQuery);
-	</script>
 @stop
--- resources/views/neon-admin/seating/print/seatmap.blade.php
+++ resources/views/neon-admin/seating/print/seatmap.blade.php
@@ -1,27 +1,35 @@
-<div class="seatmap">
-	<ul>
-		<li class="scene">Scene</li>
-		<li class="entrance" id="entrance"><p><span class="fa fa-sign-in"></span></p></li>
-
+<ul class="seatmap">
+	<li class="top-row">
+		<ul>
+			<li class="emergency-exit emergency-exit-top"><p>Nødutgang</p></li>
+			<li class="entrance"><p>INNGANG</p></li>
+		</ul>
+	</li>
+	<li class="emergency-row">
+		<ul>
+			<li class="emergency-exit emergency-exit-left"><p>Nødutgang</p></li>
+			<li class="emergency-exit emergency-exit-left gate"><p>Port</p></li>
+		</ul>
+	</li>
+	<ul class="seat-rows">
 		@foreach($rows as $row)
-
-			<li class="seat-row">
+			<li class="seat-row" @if($row->slug == 'l') style="margin-top:20px;" @endif>
 				<ul class="seat-row-{{$row->slug}}">
-					@if($row->slug == 'a')
-						<li class="seat kiosk" id="kiosk"><p><span class="fa fa-coffee"></span></p></li>
-					@endif
-					@foreach($row->seats as $seat)
 					
-						<li class="seat @if($seat->reservationsThisYear()->count() > 0) @if($seat->reservationsThisYear()->first()->status->id == 1) seat-reserved @elseif($seat->reservationsThisYear()->first()->status->id == 2) seat-tempreserved @endif @if(Sentinel::getUser()->id == $seat->reservationsThisYear()->first()->reservedfor->id and $seat->reservationsThisYear()->first()->status->id == 1) seat-yours @endif  @endif @if(Request::segment(3) == $seat->slug) active @endif ">
+					@foreach($row->seats as $seat)
+						@if($seat->slug == 'a7')
+							<li class="seat kiosk" id="kiosk"><p><span class="fa fa-coffee"></span></p></li>
+						@endif
+						<li class="seat @if($seat->reservationsThisYear()->count() > 0) @if($seat->reservationsThisYear()->first()->status->id == 1) seat-reserved @elseif($seat->reservationsThisYear()->first()->status->id == 2) seat-tempreserved @endif @if(Sentinel::getUser()->id == $seat->reservationsThisYear()->first()->reservedfor->id and $seat->reservationsThisYear()->first()->status->id == 1) seat-yours @endif  @endif @if(Request::segment(4) == $seat->slug) active @endif ">
 							<p>
 								@if($seat->reservationsThisYear()->count() == 0)
 									<a href="{{ route('admin-seating-print-seat', $seat->slug) }}" data-toggle="tooltip" title="Available">{{ $seat->name }}</a>
 								@elseif(Sentinel::getUser()->id == $seat->reservationsThisYear()->first()->reservedfor->id and $seat->reservationsThisYear()->first()->status->id == 1)
 									<a href="{{ route('admin-seating-print-seat', $seat->slug) }}" data-toggle="tooltip" title="This seat is reserved for you!">{{ $seat->name }}</a>
 								@elseif($seat->reservationsThisYear()->first()->status->id == 1)
-									<a href="{{ route('admin-seating-print-seat', $seat->slug) }}" data-toggle="tooltip" title="Reserved for: {{ User::getUsernameAndFullnameByID($seat->reservationsThisYear()->first()->reservedfor->id) }}">{{ $seat->name }}</a>
+									<a href="{{ route('admin-seating-print-seat', $seat->slug) }}" data-toggle="tooltip" title="Reserved for: {{ User::getFullnameAndNicknameByID($seat->reservationsThisYear()->first()->reservedfor->id) }}">{{ $seat->name }}</a>
 								@elseif($seat->reservationsThisYear()->first()->status->id == 2)
-									<a href="{{ route('admin-seating-print-seat', $seat->slug) }}" data-toggle="tooltip" title="Temporary Reserved By: {{ User::getUsernameAndFullnameByID($seat->reservationsThisYear()->first()->reservedfor->id) }}">{{ $seat->name }}</a>
+									<a href="{{ route('admin-seating-print-seat', $seat->slug) }}" data-toggle="tooltip" title="Temporary Reserved By: {{ User::getFullnameAndNicknameByID($seat->reservationsThisYear()->first()->reservedfor->id) }}">{{ $seat->name }}</a>
 								@else
 									{{ $seat->name }}
 								@endif
@@ -32,4 +40,10 @@
 			</li>
 		@endforeach
 	</ul>
-</div>
+	<li class="emergency-row-bottom">
+		<ul>
+			<li class="emergency-exit emergency-exit-bottom"><p>Nødutgang</p></li>
+			<li class="emergency-exit emergency-exit-bottom"><p>Nødutgang</p></li>
+		</ul>
+	</li>
+</ul>
