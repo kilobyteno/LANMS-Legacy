9d2d7598a99a7d60b2acddee211c88ca5e099acd
Daniel Billing <daniel@infihex.com>
2017-12-11 17:23:29 +0100

    Seat controller and requests

---
+++ app/Http/Requests/Admin/Seating/SeatEditRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace LANMS\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RowEditRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'     => 'required|unique:seat_rows,name|alpha|between:1,2',
+        ];
+    }
+}
