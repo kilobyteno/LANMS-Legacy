20a8c5be60f5d7919db649f4d36257eec859a93e
Daniel Billing <daniel@infihex.com>
2017-12-11 20:09:01 +0100

    Updated validation for Rows

--- app/Http/Requests/Admin/Seating/RowEditRequest.php
+++ app/Http/Requests/Admin/Seating/RowEditRequest.php
@@ -1,30 +1,30 @@
 <?php
 
-namespace LANMS\Http\Requests\Admin;
+namespace LANMS\Http\Requests\Admin\Seating;
 
 use Illuminate\Foundation\Http\FormRequest;
 
 class RowEditRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            'name'     => 'required|unique:seat_rows,name|alpha|between:1,2',
-        ];
-    }
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			'name'		=> 'required|unique:seat_rows,name,'.$this->id.',id|alpha|between:1,2',
+		];
+	}
 }
