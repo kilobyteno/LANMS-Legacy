75b88ee584bafff58505886fdadb1b275963c153
Daniel Billing <daniel@infihex.com>
2017-12-11 16:35:30 +0100

    Updated Model

--- app/SeatRows.php
+++ app/SeatRows.php
@@ -1,4 +1,6 @@
-<?php namespace LANMS;
+<?php 
+
+namespace LANMS;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -10,8 +12,23 @@ class SeatRows extends Model {
 	protected $dates = ['deleted_at'];
 	protected $table = 'seat_rows';
 
-	public function seats() {
+	protected $fillable = [
+		'name',
+		'slug',
+		'author_id',
+		'editor_id',
+	];
+
+	function seats() {
 		return $this->hasMany('Seats', 'row_id');
+	}
+
+	function author() {
+		return $this->hasOne('User', 'id', 'author_id');
+	}
+
+	function editor() {
+		return $this->hasOne('User', 'id', 'editor_id');
 	}
 
 }
