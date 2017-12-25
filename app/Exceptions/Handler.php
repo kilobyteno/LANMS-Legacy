2c8a11794b9d8905d6ee5c757da0d00d9412b190
Daniel Billing <daniel@infihex.com>
2017-12-25 17:05:29 -0500

    Why was this not here before?

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -56,7 +56,7 @@ class Handler extends ExceptionHandler {
 		// Convert all non-http exceptions to a proper 500 http exception
         // if we don't do this exceptions are shown as a default template
         // instead of our own view in resources/views/errors/500.blade.php
-        if ($this->shouldReport($exception) && !$this->isHttpException($exception)) {
+        if (!config('app.debug') && $this->shouldReport($exception) && !$this->isHttpException($exception)) {
             $exception = new HttpException(500, 'Whoops!');
         }
 
