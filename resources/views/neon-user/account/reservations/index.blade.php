737b24cffa2817edd117994021c2cf87c54598fd
Daniel Billing <daniel@infihex.com>
2018-01-14 09:20:57 +0100

    LANMS-164

--- resources/views/neon-user/account/reservations/index.blade.php
+++ resources/views/neon-user/account/reservations/index.blade.php
@@ -13,30 +13,36 @@
 				<li class="active"><strong>Reservations</strong></li>
 			</ol>
 
-			<table class="table">
-				<thead>
-					<th>ID</th>
-					<th>Date</th>
-					<th>Year</th>
-					<th>Seat</th>
-					<th>Reserved for</th>
-					<th>Reserved by</th>
-					<th>Details</th>
-				</thead>
-				<tbody>
-					@foreach($reservations as $reservation)
-						<tr>
-							<td>{{ $reservation->id }}</td>
-							<td>{{ date(User::getUserDateFormat(), strtotime($reservation->created_at)) .' at '. date(User::getUserTimeFormat(), strtotime($reservation->created_at)) }}</td>
-							<td>{{ $reservation->year or 'N/A' }}</td>
-							<td>{{ $reservation->seat->name or 'N/A' }}</td>
-							<td>{{ User::getFullnameAndNicknameByID($reservation->reservedfor->id) }}</td>
-							<td>{{ User::getFullnameAndNicknameByID($reservation->reservedby->id) }}</td>
-							<td>@if($reservation->payment)<a href="{{ route('account-billing-payment', $reservation->payment->id) }}" class="btn btn-success btn-xs btn-icon icon-left"><i class="fa fa-money"></i>View Payment</a>@endif</td>
-						</tr>
-					@endforeach
-				</tbody>
-			</table>
+			@if($reservations->count() == 0)
+				<blockquote>
+					<p><em>We can't find any data for you...</em></p>
+				</blockquote>
+			@else
+				<table class="table">
+					<thead>
+						<th>ID</th>
+						<th>Date</th>
+						<th>Year</th>
+						<th>Seat</th>
+						<th>Reserved for</th>
+						<th>Reserved by</th>
+						<th>Details</th>
+					</thead>
+					<tbody>
+						@foreach($reservations as $reservation)
+							<tr>
+								<td>{{ $reservation->id }}</td>
+								<td>{{ date(User::getUserDateFormat(), strtotime($reservation->created_at)) .' at '. date(User::getUserTimeFormat(), strtotime($reservation->created_at)) }}</td>
+								<td>{{ $reservation->year or 'N/A' }}</td>
+								<td>{{ $reservation->seat->name or 'N/A' }}</td>
+								<td>{{ User::getFullnameAndNicknameByID($reservation->reservedfor->id) }}</td>
+								<td>{{ User::getFullnameAndNicknameByID($reservation->reservedby->id) }}</td>
+								<td>@if($reservation->payment)<a href="{{ route('account-billing-payment', $reservation->payment->id) }}" class="btn btn-success btn-xs btn-icon icon-left"><i class="fa fa-money"></i>View Payment</a>@endif</td>
+							</tr>
+						@endforeach
+					</tbody>
+				</table>
+			@endif
 		</div>
 	</div>
 </div>
