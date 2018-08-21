637e65b888c4f114c89df3528ad266131ab4ddc6
Daniel Billing <daniel@infihex.com>
2018-08-21 21:38:47 +0200

    Ooooops. Conflict mistake.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ class HomeController extends Controller {
 	public function index()
 	{
 		$news = News::isPublished()->get()->take(3);
-		return view('home')->withNews($news);
+		return view('main.home')->withNews($news);
 	}
 
 }
