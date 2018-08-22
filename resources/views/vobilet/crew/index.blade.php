b1f7e23d13166fc3835859b423a2ab4323b9722c
Daniel Billing <daniel@infihex.com>
2018-08-22 19:32:43 +0200

    Crew view

---
+++ resources/views/vobilet/crew/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.main')
+@section('title', 'Crew')
+   
+@section('content')
+<div class="page-header">
+	<h4 class="page-title">Crew</h4>
+	<ol class="breadcrumb">
+		<li class="breadcrumb-item">Home</li>
+		<li class="breadcrumb-item active" aria-current="page">Crew</li>
+	</ol>
+</div>
+
+<div class="container">
+	<div class="row">
+		@foreach($crewcategories as $crewcategory)
+			<div class="col-md-6 col-lg-6 col-sm-12">
+				<h5>{{ $crewcategory->title }}</h5>
+				<div class="row">
+					@foreach($crewcategory->crewThisYear as $crew)
+						<div class="col-md-6 col-lg-6 col-sm-12">
+							<div class="card">
+								<div class="card-body d-flex flex-column">
+									<div class="text-muted">
+										@foreach($crew->skillAttachedThisYear as $skilla)
+											<small><span class="{{ $skilla->skill->label }}"><i class="{{ $skilla->skill->icon }}"></i> {{ $skilla->skill->title }}</span></small>
+										@endforeach
+									</div>
+									<div class="d-flex align-items-center pt-5 mt-auto">
+										<div class="avatar brround avatar-md mr-3" style="background-image: url(@if($crew->profilepicturesmall){{ $crew->profilepicturesmall }} @else {{ '/images/profilepicture/0_small.png' }}@endif)"></div>
+										<div><a href="{{ URL::route('user-profile', $crew->username) }}" class="text-default">{{ User::getFullnameByID($crew->id) }}</a></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					@endforeach
+				</div>
+			</div>
+		@endforeach
+	</div>
+</div>
+
+@stop
