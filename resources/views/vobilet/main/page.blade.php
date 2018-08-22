8667eef554fbd33cc3d8fa026d122bebc27ba9c6
Daniel Billing <daniel@infihex.com>
2018-08-22 19:38:38 +0200

    It should of course span across the cointainer

--- resources/views/vobilet/main/page.blade.php
+++ resources/views/vobilet/main/page.blade.php
@@ -13,7 +13,7 @@
 
 <div class="container">
 	<div class="row">
-		<div class="col-md-12 col-lg-9 col-sm-12">
+		<div class="col-md-12 col-lg-12 col-sm-12">
 			<div class="card">
 				<div class="card-body">
 					<div class="text-wrap">
