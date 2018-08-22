b11a806111b9d81af8c497d9c4e674bef362960c
Daniel Billing <daniel@infihex.com>
2018-08-22 19:32:59 +0200

    Updated the page view

--- resources/views/vobilet/main/page.blade.php
+++ resources/views/vobilet/main/page.blade.php
@@ -10,13 +10,19 @@
 	</ol>
 </div>
 
-<div class="card">
-	<div class="card-body">
-		<div class="text-wrap">
-			{!! $page->content !!}
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 col-lg-9 col-sm-12">
+			<div class="card">
+				<div class="card-body">
+					<div class="text-wrap">
+						{!! $page->content !!}
+					</div>
+				</div> 
+				<div class="card-footer">Created: {{ date(User::getUserDateFormat(), strtotime($page->created_at)) .' at '. date(User::getUserTimeFormat(), strtotime($page->created_at)) }} by <a href="{{ URL::route('user-profile', $page->author->username) }}">{{ User::getFullnameByID($page->author->id) }}</a> &middot; Updated: {{ date(User::getUserDateFormat(), strtotime($page->updated_at))  .' at '. date(User::getUserTimeFormat(), strtotime($page->updated_at)) }} by <a href="{{ URL::route('user-profile', $page->editor->username) }}">{{ User::getFullnameByID($page->editor->id) }}</a></div>
+			</div>
 		</div>
-	</div> 
-	<div class="card-footer">Created: {{ date(User::getUserDateFormat(), strtotime($page->created_at)) .' at '. date(User::getUserTimeFormat(), strtotime($page->created_at)) }} by <a href="{{ URL::route('user-profile', $page->author->username) }}">{{ User::getFullnameByID($page->author->id) }}</a> &middot; Updated: {{ date(User::getUserDateFormat(), strtotime($page->updated_at))  .' at '. date(User::getUserTimeFormat(), strtotime($page->updated_at)) }} by <a href="{{ URL::route('user-profile', $page->editor->username) }}">{{ User::getFullnameByID($page->editor->id) }}</a></div>
+	</div>
 </div>
 
 @stop
