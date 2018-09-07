6eba729673174a2351e99ea2b5371b965425ee62
Daniel Billing <daniel@infihex.com>
2018-09-07 21:35:08 +0200

    dialect/laravel-gdpr-compliance fix for namespace

--- vendor/dialect/laravel-gdpr-compliance/src/GdprServiceProvider.php
+++ vendor/dialect/laravel-gdpr-compliance/src/GdprServiceProvider.php
@@ -36,7 +36,7 @@ class GdprServiceProvider extends ServiceProvider
     {
         Route::group([
             'prefix' => config('gdpr.uri'),
-            'namespace' => 'App\Http\Controllers',
+            'namespace' => app()->getNamespace().'Http\Controllers',
             'middleware' => config('gdpr.middleware'),
         ], function () {
             $this->loadRoutesFrom(__DIR__.'/routes/web.php');
