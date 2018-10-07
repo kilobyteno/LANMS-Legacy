d6e8923f9b03161d4176cd9ac8473690bca480ba
Daniel Billing <daniel@infihex.com>
2018-10-07 21:06:36 +0200

    Removed setting from menu

--- resources/views/vobilet/layouts/main.blade.php
+++ resources/views/vobilet/layouts/main.blade.php
@@ -62,9 +62,6 @@
 											<a class="dropdown-item" href="{{ route('account') }}">
 												<i class="dropdown-icon  mdi mdi-settings"></i> Account
 											</a>
-											<a class="dropdown-item" href="{{ route('account-settings') }}">
-												<i class="dropdown-icon  mdi mdi-settings"></i> Settings
-											</a>
 											<div class="dropdown-divider"></div>
 											<a class="dropdown-item" href="{{ route('logout') }}">
 												<i class="dropdown-icon mdi  mdi-logout-variant"></i> Sign out
