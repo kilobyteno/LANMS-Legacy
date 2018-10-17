8b225f95d9c0dcdd8309e3881427a3aae2a05359
Daniel Billing <daniel@infihex.com>
2018-10-17 15:23:10 +0200

    Updated requests with max file size

--- app/Http/Requests/Member/ProfileCoverRequest.php
+++ app/Http/Requests/Member/ProfileCoverRequest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Http\FormRequest;
 class ProfileCoverRequest extends FormRequest {
 	public function rules() {
 		return [
-			'profilecover' => 'image|size:5000',
+			'profilecover' => 'image|max:25000',
 		];
 	}
 	
--- app/Http/Requests/Member/ProfileImageRequest.php
+++ app/Http/Requests/Member/ProfileImageRequest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Http\FormRequest;
 class ProfileImageRequest extends FormRequest {
 	public function rules() {
 		return [
-			'profileimage' => 'image|size:5000',
+			'profileimage' => 'image|max:25000',
 		];
 	}
 	
