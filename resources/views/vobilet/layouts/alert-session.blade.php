f4ea31c3ec0e88750d3ca07dfcdb6d2a6ad0b17d
Daniel Billing <daniel@infihex.com>
2018-10-24 19:46:33 +0200

    Updated icon in alert

--- resources/views/vobilet/layouts/alert-session.blade.php
+++ resources/views/vobilet/layouts/alert-session.blade.php
@@ -2,7 +2,7 @@
 	<div class="alert alert-{{ Session::get('messagetype') }}" role="alert">
 		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 		@if(Session::get('messagetype') == 'info')
-			<i class="fas fa-bell mr-2" aria-hidden="true"></i>
+			<i class="fas fa-info mr-2" aria-hidden="true"></i>
 		@elseif(Session::get('messagetype') == 'warning')
 			<i class="fas fa-exclamation mr-2" aria-hidden="true"></i>
 		@elseif(Session::get('messagetype') == 'danger')
