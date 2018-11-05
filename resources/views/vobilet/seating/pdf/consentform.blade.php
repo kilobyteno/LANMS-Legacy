99c799035d2496b1899e4601bffc703dc130883a
Daniel Billing <daniel@infihex.com>
2018-11-05 21:21:31 +0100

    Oppdatert samtykkeskjema

--- resources/views/vobilet/seating/pdf/consentform.blade.php
+++ resources/views/vobilet/seating/pdf/consentform.blade.php
@@ -23,25 +23,49 @@
 				<img src=".{{ Setting::get('WEB_LOGO') }}" style="width:auto;height:auto;max-width:700px;max-height:125px;">
 				<h1 class="text-center">Samtykkeskjema<br><small>{{ Setting::get('WEB_NAME') }} {{ Setting::get('SEATING_YEAR') }}</small></h1>
 				<hr>
-				<p>Alle deltakere under 16 år må ha med seg skriftlig tillatelse fra foresatte til å være med på arrangementet. Mindreårige deltakere som ikke har tillatelse er vi dessverre nødt til å avvise. Downlink har ingen aldersgrense, men vi anbefaler at deltakere under 16 år ikke kommer alene.</p>
-				<p>Vi kan ikke gi noen personlig garanti under arrangementet, men vi gjør vårt ytterste for å sørge for at deltakerne får en minnerik vinterferie. </p>
+				<p>Alle deltakere under 16 år må ha med seg skriftlig tillatelse fra omsorgsperson til å være med på arrangementet. Mindreårige deltakere som ikke har tillatelse er vi dessverre nødt til å avvise.</p>
+				<p>Aldersgrensen for å delta på arrangementet er satt til 13 år, er deltaker under denne grensen må omsorgsperson være med på arrengementet.</p>
+				<p>Arrangørene ønsker retten til:</p>
+				<ul>
+					<li>Å publisere bilde/video av deltaker på arrangementet sin nettside/sosialemedier. Åpen Facebook-side og åpen nettside. Når deltaker har kommet på pallplassering i konkurranser og/eller er med på oversiktlige bilder av arrangementet.</li>
+					<li>Å kun ha gyldighet på dette samtykkeskjemaet i uken arrangementet skjer</li>
+				</ul>
+				<p>Det bør også nevnes at arrangørene;</p>
+				<ul>
+					<li>Vil ikke ta bilder av barna når de er svært lettkledde.</li>
+					<li>Vil gjøre sitt beste for at bildene som publiseres på nett vil legges ut i en kvalitet som gjør det lite attraktivt for andre å manipulere dem eller benytte dem i andre sammenhenger.</li>
+					<li>Vil alltid be om spesifikt samtykke fra omsorgsperson dersom det skal tas bilder av barna til journalistiske formål, eller dersom utplasserte studenter ønsker å ta bilder av barna.</li>
+					<li>Vil ikke frigi digitale bildekopier av barnet uten spesifikt samtykke fra de foresatte.</li>
+				</ul>
 				<p>
-					<strong>Informasjon til foreldre</strong>
+					<strong>Informasjon til omsorgsperson</strong>
 					<br>
-					Vi har samlet informasjon til foreldre på nettsidene våre;
+					Vi har samlet informasjon til omsorgsperson på nettsidene våre:
 					<br>
 					<strong><em>{{ Setting::get('WEB_PROTOCOL') }}://{{ Setting::get('WEB_DOMAIN') }}@if(Setting::get('WEB_PORT') <> 80){{ ':'.Setting::get('WEB_PORT') }}@endif/tilforeldre</em></strong>
 				</p>
 				<hr>
 				<h2>Tillatelse til å delta på {{ Setting::get('WEB_NAME') }} {{ Setting::get('SEATING_YEAR') }}</h2>
 				<p>Jeg gir med dette tillatelse til at <strong>{{ strtoupper(Sentinel::getUser()->firstname . ' ' . Sentinel::getUser()->lastname) }}</strong> som er født @if(Sentinel::getUser()->birthdate)<strong>{{ Sentinel::getUser()->birthdate }}</strong>@else{{ '__________________' }}@endif kan delta på arrangementet &quot;{{ Setting::get('WEB_NAME') }} {{ Setting::get('SEATING_YEAR') }}&quot; i {{ LANMS\Info::getContent('where') }} {{ LANMS\Info::getContent('when') }}.</p>
+				<p>Jeg gir med dette tillatelse til arrangørene som nevnt i punkt(ene) over.</p>
 				<p>Mitt forhold til deltakeren (sett strek under); <strong><em>mor</em></strong>, <strong><em>far</em></strong> eller <strong><em>annen omsorgsperson</em></strong> (spesifiser under).<br>______________________________________________________________________</p>
-				<p>Kontaktinformasjon til omsorgsperson (om det skulle bli nødvendig):</p>
-				<p>Fulltnavn: ____________________________________________________________________<br>Telefonnummer: _________________________________</p>
+				<p>Kontaktinformasjon til omsorgsperson, om det skulle bli nødvendig (<strong>skriv tydelig</strong>):</p>
+				<p>Fulltnavn: ____________________________________________________________________<br>Telefonnummer: _________________________________<br>Epost: _________________________________</p>
 				<p>Jeg kan treffes på telefon hele døgnet om det skulle oppstå en nødsituasjon og jeg er inneforstått med at arrangøren kan ringe for å kontrollere gyldigheten av denne tillatelsen.</p>
 				<div class="row">
+					<p>Fylles ut av <strong>omsorgsperson</strong>:</p>
 					<div style="width:50%;float:left;display:inline-block">
 						<p>Sted og dato:<br><br>_________________________________________________</p>
+					</div>
+					<div style="width:50%;float:right;display:inline-block">
+						<p>Signatur:<br><br>_________________________________________________</p>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<p>Fylles ut av <em>arrangørene</em>:</p>
+					<div style="width:50%;float:left;display:inline-block">
+						<p>Dato:<br><br>_________________________________________________</p>
 					</div>
 					<div style="width:50%;float:right;display:inline-block">
 						<p>Signatur:<br><br>_________________________________________________</p>
