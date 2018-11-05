cbe0f3a25cc4bcd1b4f9b8451c4800d11ee67b48
Daniel Billing <daniel@infihex.com>
2018-11-05 20:27:26 +0100

    Updated Font Awesome

--- resources/views/vobilet/layouts/main.blade.php
+++ resources/views/vobilet/layouts/main.blade.php
@@ -17,7 +17,8 @@
 
 		<!-- Title -->
 		<title>@yield('title') - {{ Setting::get('WEB_NAME') }}</title>
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">		
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
 		<!-- Font Family-->
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
 
