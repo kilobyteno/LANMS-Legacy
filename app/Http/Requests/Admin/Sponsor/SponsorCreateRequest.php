58ee786c770b65e34421751239b44030e2427e8d
Daniel Billing <daniel@infihex.com>
2018-11-12 21:08:49 +0100

    PSR-2

--- app/Http/Requests/Admin/Sponsor/SponsorCreateRequest.php
+++ app/Http/Requests/Admin/Sponsor/SponsorCreateRequest.php
@@ -1,20 +1,23 @@
 <?php
 
 namespace LANMS\Http\Requests\Admin\Sponsor;
- 
+
 use Illuminate\Foundation\Http\FormRequest;
- 
-class SponsorCreateRequest extends FormRequest {
-	public function rules() {
-		return [
-			'name' 			=> 'required',
-			'url' 			=> '',
-			'description' 	=> '',
-			'image' 		=> 'required|image|mimes:jpeg,png,jpg,gif,svg',
-		];
-	}
-	
-	public function authorize() {
-		return true;
-	}
+
+class SponsorCreateRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'name' 			=> 'required',
+            'url' 			=> '',
+            'description' 	=> '',
+            'image' 		=> 'required|image|mimes:jpeg,png,jpg,gif,svg',
+        ];
+    }
+    
+    public function authorize()
+    {
+        return true;
+    }
 }
