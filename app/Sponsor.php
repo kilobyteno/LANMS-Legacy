753f88e75f0ac2f0ec325b8b54225c9773257c7e
Daniel Billing <daniel@infihex.com>
2018-11-12 21:15:19 +0100

    PSR-2

--- app/Sponsor.php
+++ app/Sponsor.php
@@ -7,34 +7,36 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Sponsor extends Model
 {
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+    protected $fillable = [
+        'name',
+        'url',
+        'description',
+        'image',
+        'author_id',
+        'editor_id',
+    ];
+    protected $table = 'sponsors';
+
+    public function author()
+    {
+        return $this->hasOne('User', 'id', 'author_id');
+    }
+
+    public function editor()
+    {
+        return $this->hasOne('User', 'id', 'editor_id');
+    }
     
-	use SoftDeletes;
+    public function scopeThisYear($query)
+    {
+        return $query->where('year', '=', \Setting::get('SEATING_YEAR'));
+    }
 
-	protected $dates = ['deleted_at'];
-	protected $fillable = [
-		'name',
-		'url',
-		'description',
-		'image',
-		'author_id',
-		'editor_id',
-	];
-	protected $table = 'sponsors';
-
-	function author() {
-		return $this->hasOne('User', 'id', 'author_id');
-	}
-
-	function editor() {
-		return $this->hasOne('User', 'id', 'editor_id');
-	}
-	
-	public function scopeThisYear($query) {
-		return $query->where('year', '=', \Setting::get('SEATING_YEAR'));
-	}
-
-	public function scopeLastYear($query) {
-		return $query->where('year', '<', \Setting::get('SEATING_YEAR'));
-	}
-
+    public function scopeLastYear($query)
+    {
+        return $query->where('year', '<', \Setting::get('SEATING_YEAR'));
+    }
 }
