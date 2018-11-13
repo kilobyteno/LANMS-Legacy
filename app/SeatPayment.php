009ac7d05f3fa4c7d3650f7efe6870aeba861381
Daniel Billing <daniel@infihex.com>
2018-11-13 22:33:59 +0100

    PSR-2

--- app/SeatPayment.php
+++ app/SeatPayment.php
@@ -1,23 +1,26 @@
-<?php namespace LANMS;
+<?php
+
+namespace LANMS;
 
 use Illuminate\Database\Eloquent\Model;
 
-class SeatPayment extends Model {
+class SeatPayment extends Model
+{
+    protected $table = 'seat_payments';
 
-	protected $table = 'seat_payments';
+    protected $fillable = [
+        'stripecharge',
+        'user_id',
+        'reservation_id',
+    ];
 
-	protected $fillable = [
-		'stripecharge',
-		'user_id',
-		'reservation_id',
-	];
+    public function user()
+    {
+        return $this->hasOne('User', 'id', 'user_id');
+    }
 
-	function user() {
-		return $this->hasOne('User', 'id', 'user_id');
-	}
-
-	function reservation() {
-		return $this->hasOne('SeatReservation', 'id', 'reservation_id');
-	}
-
+    public function reservation()
+    {
+        return $this->hasOne('SeatReservation', 'id', 'reservation_id');
+    }
 }
