e60753b4e0d30ac1a942bedbeeabefabaad15e9e
Daniel Billing <daniel@infihex.com>
2018-11-25 16:31:10 +0100

    PSR-2 changes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,85 +6,84 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Session\TokenMismatchException;
 
-class Handler extends ExceptionHandler {
+class Handler extends ExceptionHandler
+{
 
-	/**
-	 * A list of the exception types that should not be reported.
-	 *
-	 * @var array
-	 */
-	protected $dontReport = [
-		\Illuminate\Auth\AuthenticationException::class,
-		\Illuminate\Auth\Access\AuthorizationException::class,
-		\Symfony\Component\HttpKernel\Exception\HttpException::class,
-		\Illuminate\Database\Eloquent\ModelNotFoundException::class,
-		\Illuminate\Session\TokenMismatchException::class,
-		\Illuminate\Validation\ValidationException::class,
-	];
-	private $sentryID;
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        \Illuminate\Auth\AuthenticationException::class,
+        \Illuminate\Auth\Access\AuthorizationException::class,
+        \Symfony\Component\HttpKernel\Exception\HttpException::class,
+        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
+        \Illuminate\Session\TokenMismatchException::class,
+        \Illuminate\Validation\ValidationException::class,
+    ];
+    private $sentryID;
 
-	/**
-	 * Report or log an exception.
-	 *
-	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-	 *
-	 * @param  \Exception  $exception
-	 * @return void
-	 */
-	public function report(Exception $exception)
-	{
-		if (!config('app.debug') && app()->bound('sentry') && $this->shouldReport($exception)) {
-			app('sentry')->captureException($exception);
-		}
-		parent::report($exception);
-	}
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception  $exception
+     * @return void
+     */
+    public function report(Exception $exception)
+    {
+        if (!config('app.debug') && app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
+        parent::report($exception);
+    }
 
 
-	/**
-	 * Render an exception into an HTTP response.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Exception  $exception
-	 * @return \Illuminate\Http\Response
-	 */
-	public function render($request, Exception $exception)
-	{
-		\Theme::set('vobilet');
-		if($exception instanceof NotFoundHttpException)
-		{
-			return view('errors.404');
-		}
-
-		if ($exception instanceof TokenMismatchException) {
-            //Redirect to login form if session expires
-            return redirect()->back()
-			            ->with('messagetype', 'danger')
-						->with('message', 'Validation Token has expired. Please try again!');
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $exception)
+    {
+        \Theme::set('vobilet');
+        if ($exception instanceof NotFoundHttpException) {
+            return view('errors.404');
         }
 
-		// Convert all non-http exceptions to a proper 500 http exception
+        if ($exception instanceof TokenMismatchException) {
+            //Redirect to login form if session expires
+            return redirect()->back()
+                        ->with('messagetype', 'danger')
+                        ->with('message', 'Validation Token has expired. Please try again!');
+        }
+
+        // Convert all non-http exceptions to a proper 500 http exception
         // if we don't do this exceptions are shown as a default template
         // instead of our own view in resources/views/errors/500.blade.php
         if (!config('app.debug') && $this->shouldReport($exception) && !$this->isHttpException($exception)) {
             $exception = new HttpException(500, 'Whoops!');
         }
 
-		return parent::render($request, $exception);
-	}
-	
-	/**
-	 * Convert an authentication exception into an unauthenticated response.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Illuminate\Auth\AuthenticationException  $exception
-	 * @return \Illuminate\Http\Response
-	 */
-	protected function unauthenticated($request, AuthenticationException $exception)
-	{
-		if ($request->expectsJson()) {
-			return response()->json(['error' => 'Unauthenticated.'], 401);
-		}
-		return redirect()->guest(route('login'));
-	}
-
+        return parent::render($request, $exception);
+    }
+    
+    /**
+     * Convert an authentication exception into an unauthenticated response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['error' => 'Unauthenticated.'], 401);
+        }
+        return redirect()->guest(route('login'));
+    }
 }
