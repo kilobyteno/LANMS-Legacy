97da86cc102827ac9891341af7fbb06a039689f2
Daniel Billing <daniel@infihex.com>
2018-12-04 09:26:58 +0100

    LANMS-215

--- resources/views/vobilet/errors/500.blade.php
+++ resources/views/vobilet/errors/500.blade.php
@@ -26,7 +26,7 @@
             eventId: '{{ Sentry::getLastEventID() }}',
             dsn: 'https://e9ebbd88548a441288393c457ec90441@sentry.io/3235',
             user.email: '{{ \Sentinel::getUser()->email ?? "" }}',
-            user.name: '{{ User::getFullnameByID(\Sentinel::getUser()->id) ?? "" }}'
+            user.name: '{{ \LANMS\User::getFullnameByID(\Sentinel::getUser()->id) ?? "Unknown" }}'
         });
     </script>
 @endif
