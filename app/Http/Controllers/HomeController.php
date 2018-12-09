1914a7f07a61937257fb565f2c621e949adb3139
Daniel Billing <daniel@infihex.com>
2018-12-09 12:54:02 +0100

    PSR-2 changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,15 +1,17 @@
-<?php namespace LANMS\Http\Controllers;
+<?php
+
+namespace LANMS\Http\Controllers;
 
 use LANMS\Page;
 use LANMS\News;
 
-class HomeController extends Controller {
-
-	public function index()
-	{
-		$news = News::isPublished()->get()->take(4);
-		return view('main.home')->withNews($news);
-	}
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $news = News::isPublished()->get()->take(4);
+        return view('main.home')->withNews($news);
+    }
 
     public function locale($locale)
     {
