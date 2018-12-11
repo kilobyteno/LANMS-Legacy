31cf460356ffd9680922cd7d3f2feb8ad76a1ab3
Daniel Billing <daniel@infihex.com>
2018-12-11 20:58:49 +0100

    LANMS-217

--- resources/views/vobilet/account/changeimages.blade.php
+++ resources/views/vobilet/account/changeimages.blade.php
@@ -77,3 +77,12 @@
 	</div>
 </div>
 @stop
+
+@section('javascript')
+	<script type="text/javascript">
+		$('.custom-file-input').on('change', function() { 
+            let fileName = $(this).val().split('\\').pop(); 
+            $(this).next('.custom-file-label').addClass("selected").html(fileName); 
+		});
+	</script>
+@stop
