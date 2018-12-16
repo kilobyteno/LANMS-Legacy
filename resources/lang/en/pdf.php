6a5df41c9c9ed88bc02f633a089bf9ca43e5b54d
Daniel Billing <daniel@infihex.com>
2018-12-16 19:45:03 +0100

    Added translation for revceipt and ticket

---
+++ resources/lang/en/pdf.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'receipt' => array(
+        'reservationnumber' => 'Reservation #',
+        'paymentmethod' => 'Payment Method',
+        'paymentinfo' => 'Payment Info',
+        'card' => 'Card',
+        'item' => 'Item',
+        'price' => 'Price',
+        'ticket' => 'Ticket',
+        'total' => 'Total',
+    ),
+
+    'ticket' => array(
+        'title' => 'Ticket',
+        'desc' => 'This is your ticket, for one seat, bring this to the event. Remember that there is one ticket per seat! It is also important that you bring valid identification. If you show up without a ticket or credentials, we cannot check you in to the event. If you need a consent form, remember to bring that as well.',
+        'moreinfo' => 'For more information, go to our website',
+        'paymentinfo' => 'Payment Info',
+        'yourseat' => 'Your seat',
+    ),
+
+];
