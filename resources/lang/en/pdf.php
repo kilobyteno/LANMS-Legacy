8e3503d1ed690d773910ac94f871bb7d328747bc
Daniel Billing <daniel@infihex.com>
2018-12-18 20:57:45 +0100

    Consent form translated

--- resources/lang/en/pdf.php
+++ resources/lang/en/pdf.php
@@ -25,14 +25,23 @@ return [
         'title' => 'Samtykkeskjema',
         'desc' => 'Alle deltakere under 16 år må ha med seg skriftlig tillatelse fra omsorgsperson til å være med på arrangementet. Mindreårige deltakere som ikke har tillatelse er vi dessverre nødt til å avvise. Aldersgrensen for å delta på arrangementet er satt til 13 år, er deltaker under denne grensen må omsorgsperson være med på arrengementet.',
         'organizerswantright' => 'Arrangørene ønsker retten til',
-        'organizerswantright1' => 'Å publisere bilde/video av deltaker på arrangementet sin nettside/sosialemedier. Åpen Facebook-side og åpen nettside. Når deltaker har kommet på pallplassering i konkurranser og/eller er med på oversiktlige bilder av arrangementet.',
-        'organizerswantright2' => 'Å kun ha gyldighet på dette samtykkeskjemaet i uken arrangementet skjer',
+        'organizerswantrightpoints' => '<li>Å publisere bilde/video av deltaker på arrangementet sin nettside/sosialemedier. Åpen Facebook-side og åpen nettside. Når deltaker har kommet på pallplassering i konkurranser og/eller er med på oversiktlige bilder av arrangementet.</li>
+                    <li>Å kun ha gyldighet på dette samtykkeskjemaet i uken arrangementet skjer</li>',
         'shouldalsobementioned' => 'Det bør også nevnes at arrangørene',
-        'shouldalsobementioned1' => 'Vil ikke ta bilder av barna når de er svært lettkledde.',
-        'shouldalsobementioned2' => 'Vil gjøre sitt beste for at bildene som publiseres på nett vil legges ut i en kvalitet som gjør det lite attraktivt for andre å manipulere dem eller benytte dem i andre sammenhenger.',
-        'shouldalsobementioned3' => 'Vil alltid be om spesifikt samtykke fra omsorgsperson dersom det skal tas bilder av barna til journalistiske formål, eller dersom utplasserte studenter ønsker å ta bilder av barna.',
-        'shouldalsobementioned4' => 'Vil ikke frigi digitale bildekopier av barnet uten spesifikt samtykke fra de foresatte.',
-        'moreinfo' => 'Vi har samlet informasjon til omsorgsperson på nettsidene våre',
+        'shouldalsobementionedpoints' => '<li>Vil ikke ta bilder av barna når de er svært lettkledde.</li>
+                    <li>Vil gjøre sitt beste for at bildene som publiseres på nett vil legges ut i en kvalitet som gjør det lite attraktivt for andre å manipulere dem eller benytte dem i andre sammenhenger.</li>
+                    <li>Vil alltid be om spesifikt samtykke fra omsorgsperson dersom det skal tas bilder av barna til journalistiske formål, eller dersom utplasserte studenter ønsker å ta bilder av barna.</li>
+                    <li>Vil ikke frigi digitale bildekopier av barnet uten spesifikt samtykke fra de foresatte.</li>',
+        'moreinfo' => 'Vi har samlet informasjon til omsorgsperson på nettsidene våre: <br><strong><em>:url</em></strong>',
+        'permission' => 'Tillatelse til å delta på :event',
+        'permissiondesc' => 'Jeg gir med dette tillatelse til at :name som er født :birthdate kan delta på arrangementet ":event" i :location. Jeg gir med dette tillatelse til arrangørene som nevnt i punkt(ene) over.',
+        'myrelationship' => 'Mitt forhold til deltakeren (sett strek under); <strong><em>mor</em></strong>, <strong><em>far</em></strong> eller <strong><em>annen omsorgsperson</em></strong> (spesifiser under).',
+        'contact' => 'Kontaktinformasjon til omsorgsperson, om det skulle bli nødvendig (<strong>skriv tydelig</strong>)',
+        'icanbecontacted' => 'Jeg kan treffes på telefon hele døgnet om det skulle oppstå en nødsituasjon og jeg er inneforstått med at arrangøren kan ringe for å kontrollere gyldigheten av denne tillatelsen.',
+        'filledoutbycaregiver' => 'Fylles ut av <strong>omsorgsperson</strong>',
+        'filledoutbyorganizers' => 'Fylles ut av <em>arrangørene</em>',
+        'placeanddate' => 'Sted og dato',
+        'signature' => 'Signatur',
     ),
 
 ];
