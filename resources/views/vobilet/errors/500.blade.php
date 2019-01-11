16631e3de7b00e37a1b9a5b5632d5a54f715d528
Daniel Billing <daniel@infihex.com>
2019-01-11 10:06:33 +0100

    LANMS-222

--- resources/views/vobilet/errors/500.blade.php
+++ resources/views/vobilet/errors/500.blade.php
@@ -6,7 +6,7 @@
     <div class="page-header">
         <h4 class="page-title">Server Error</h4>
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+            <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
             <li class="breadcrumb-item active" aria-current="page">Server Error</li>
         </ol>
     </div>
@@ -15,7 +15,7 @@
             <div class="display-1 text-dark mb-5">500</div>
             <h1>Looks like we're having some server issues.</h1>
             <p>Error ID: {{ Sentry::getLastEventID() ?? 'N/A' }}</p>
-            <p><a class="btn btn-primary mt-10" href="{{ route('home') }}"><i class="fas fa-arrow-left"></i> Back To Home</a></p>
+            <p><a class="btn btn-primary mt-10" href="{{ url('/') }}"><i class="fas fa-arrow-left"></i> Back To Home</a></p>
         </div>      
     </div>
 </div>
