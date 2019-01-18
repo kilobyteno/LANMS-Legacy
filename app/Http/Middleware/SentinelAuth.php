11a708d56e8b87cc74bf1d569093b04c1f60152e
Daniel Billing <daniel@infihex.com>
2019-01-18 21:32:29 +0100

    PSR-2

--- app/Http/Middleware/SentinelAuth.php
+++ app/Http/Middleware/SentinelAuth.php
@@ -1,38 +1,39 @@
-<?php namespace LANMS\Http\Middleware;
+<?php
+
+namespace LANMS\Http\Middleware;
 
 use Closure;
 use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
 
-class SentinelAuth {
+class SentinelAuth
+{
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
-		if (Sentinel::guest())
-		{
-			if ($request->ajax()) {
-				return response('Unauthorized.', 401);
-			} else {
-				return redirect()->route('account-signin')
-										->with('messagetype', 'info')
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Sentinel::guest()) {
+            if ($request->ajax()) {
+                return response('Unauthorized.', 401);
+            } else {
+                return redirect()->route('account-signin')
+                                        ->with('messagetype', 'info')
                                         ->with('message', trans('global.noaccess'));
-			}
-		} else {
-			$now = date_create('now');
-			$user = Sentinel::findById(Sentinel::getUser()->id);
-			$details = [
-				'last_activity' => $now
-			];
-			Sentinel::update($user, $details);
-		}
+            }
+        } else {
+            $now = date_create('now');
+            $user = Sentinel::findById(Sentinel::getUser()->id);
+            $details = [
+                'last_activity' => $now
+            ];
+            Sentinel::update($user, $details);
+        }
 
-		return $next($request);
-	}
-
+        return $next($request);
+    }
 }
