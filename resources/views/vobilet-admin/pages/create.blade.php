b3af4f6b7d0aa983d6cac89854719caed7dc76b2
Daniel Billing <daniel@infihex.com>
2019-01-26 21:38:48 +0100

    Pages

---
+++ resources/views/vobilet-admin/pages/create.blade.php
@@ -0,0 +1,125 @@
+@extends('layouts.main')
+@section('title', 'Create Page - Admin')
+@section('content')
+
+<div class="page-header">
+	<h4 class="page-title">Create Page</h4>
+	<ol class="breadcrumb">
+		<li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+		<li class="breadcrumb-item"><a href="{{ route('admin') }}">Admin</a></li>
+		<li class="breadcrumb-item"><a href="{{ route('admin-pages') }}">Pages</a></li>
+		<li class="breadcrumb-item active" aria-current="page">Create Page</li>
+	</ol>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+
+		<div class="card">
+			<div class="card-body">
+				<form action="{{ route('admin-pages-store') }}" method="post">
+
+					<div class="input-group mb-5">
+						<input type="text" class="form-control input-lg" name="title" autocomplete="off" placeholder="Page title" value="{{ (old('title')) ? old('title') : '' }}" />
+						<span class="input-group-append">
+							<button class="btn btn-success" type="submit"><i class="fa fa-save mr-2"></i> Save</button>
+						</span>
+						@if($errors->has('title'))
+							<p class="text-danger">{{ $errors->first('title') }}</p>
+						@endif
+					</div>
+
+					<!-- WYSIWYG - Content Editor -->
+					<div class="row">
+						<div class="col-sm-12 @if($errors->has('content')) has-error @endif">
+							<textarea class="content richText-initial" name="content">
+								{{ (old('content')) ? old('content') : '' }}
+							</textarea>
+							@if($errors->has('content'))
+								<p class="text-danger">{{ $errors->first('content') }}</p>
+							@endif
+						</div>
+					</div>
+					
+					<br />
+
+					<!-- Metaboxes -->
+					<div class="row">
+						
+						<div class="col-sm-4">
+							<div class="expanel expanel-default @if($errors->has('active')) expanel-danger @endif" data-collapsed="0">
+								<div class="expanel-heading">
+									<div class="expanel-title">Page Settings</div>
+								</div>
+								<div class="expanel-body">
+									<div class="@if($errors->has('active')) text-danger @endif">
+										<label class="custom-control custom-checkbox">
+											<input type="checkbox" class="custom-control-input" name="active" checked="">
+											<span class="custom-control-label">Active</span>
+										</label>
+										<p><small><em>If unchecked the page won't be visible or accessible anywhere</em></small></p>
+										@if($errors->has('active'))
+											<p class="text-danger">{{ $errors->first('active') }}</p>
+										@endif
+									</div>
+									<div class="@if($errors->has('showinmenu')) text-danger @endif">
+										<label class="custom-control custom-checkbox">
+											<input type="checkbox" class="custom-control-input" name="showinmenu" checked="">
+											<span class="custom-control-label">Show in Menu</span>
+										</label>
+										<p><small><em>If checked the page will be visible in the menu on the frontend</em></small></p>
+										@if($errors->has('showinmenu'))
+											<p class="text-danger">{{ $errors->first('showinmenu') }}</p>
+										@endif
+									</div>	
+								</div>
+							</div>
+						</div>
+						
+						<div class="col-sm-4">
+							<div class="expanel expanel-default @if($errors->has('slug')) expanel-danger @endif" data-collapsed="0">
+								<div class="expanel-heading">
+									<div class="expanel-title">Slug <small class="text-muted">*Optional</small></div>
+								</div>
+								<div class="expanel-body">
+									<div class="input-group">
+										<div class="input-group-prepend">
+											<div class="input-group-text">
+												<i class="fa fa-heading"></i>
+											</div>
+										</div>
+										<input type="text" class="form-control" value="{{ (old('slug')) ? old('slug') : '' }}" name="slug" autocomplete="off">
+									</div>
+									<br>
+									<p><em>Example: info</em></p>
+									@if($errors->has('slug'))
+										<p class="text-danger">{{ $errors->first('slug') }}</p>
+									@endif
+								</div>
+							</div>
+						</div>
+						
+					</div>
+
+					<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+				</form>
+			</div>
+		</div>
+
+	</div>
+</div>
+
+@stop
+@section('css')
+	<link href="{{ Theme::url('plugins/wysiwyag/richtext.min.css') }}" rel="stylesheet">
+@stop
+@section('javascript')
+	<script src="{{ Theme::url('plugins/wysiwyag/jquery.richtext.js') }}"></script>
+	<script type="text/javascript">
+		$(function(e) {
+			$('.content').richText();
+			$('.richText-help').remove(); // Remove useless help icon
+		});
+	</script>
+@stop
---
+++ resources/views/vobilet-admin/pages/index.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.main')
+@section('title', 'Pages - Admin')
+@section('content')
+
+<div class="page-header">
+	<h4 class="page-title">Pages @if(Sentinel::hasAccess('admin.pages.create'))<a class="btn btn-sm btn-success ml-2" href="{{ route('admin-pages-create') }}"><i class="fa fa-plus mr-2"></i> Create</a>@endif</h4>
+	<ol class="breadcrumb">
+		<li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+		<li class="breadcrumb-item"><a href="{{ route('admin') }}">Admin</a></li>
+		<li class="breadcrumb-item active" aria-current="page">Pages</li>
+	</ol>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+		
+		<table class="table table-striped table-bordered dataTable no-footer" id="table-1">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Slug</th>
+					<th>Title</th>
+					<th>Created at</th>
+					<th>Created by</th>
+					<th>Edited at</th>
+					<th>Edited by</th>
+					<th>Actions</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($pages as $page)
+					<tr>
+						<th scope="row">{{ $page->id }}</th>
+						<td>{{ $page->slug }}</td>
+						<td>{{ $page->title }}</td>
+						<td>{{ ucfirst(\Carbon::parse($page->created_at)->isoFormat('LLL')) }}</td>
+						<td><a href="{{ URL::route('user-profile', $page->author->username) }}">{{ User::getFullnameByID($page->author->id) }}</a></td>
+						<td>@if($page->edited_at){{ ucfirst(\Carbon::parse($page->edited_at)->isoFormat('LLL')) }}@endif</td>
+						<td>@if($page->editor->username)<a href="{{ URL::route('user-profile', $page->editor->username) }}">{{ User::getFullnameByID($page->editor->id) }}</a>@endif</td>
+						<td>
+							<a href="{{ route('page', $page->slug) }}" class="btn btn-info btn-sm"><i class="fa fa-eye mr-2"></i>View</a>
+							<a href="{{ route('admin-pages-edit', $page->id) }}" class="btn btn-warning btn-sm"><i class="fa fa-edit mr-2"></i>Edit</a>
+							@if(Sentinel::hasAccess('admin.pages.destroy'))
+								<a href="javascript:;" onclick="jQuery('#page-destroy-{{ $page->id }}').modal('show', {backdrop: 'static'});" class="btn btn-danger btn-sm"><i class="fa fa-trash mr-2"></i>Delete</a>
+							@endif
+						</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+
+	</div>
+</div>
+
+@foreach($pages as $page)
+	<div class="modal fade" id="page-destroy-{{ $page->id }}" data-backdrop="static">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title"><strong>Delete Page:</strong> #{{ $page->id }} - {{ $page->title }}</h4>
+				</div>
+				<div class="modal-body">
+					<h4 class="text-danger text-center"><strong>Are you sure you want to delete this page?</strong></h4>
+				</div>
+				<div class="modal-footer">
+					<a href="{{ route('admin-pages-destroy', $page->id) }}" class="btn btn-danger">Yes, I want to delete it.</a>
+					<button type="button" class="btn btn-success" data-dismiss="modal">No, take me away!</button>
+				</div>
+			</div>
+		</div>
+	</div>
+@endforeach
+
+@stop
+
+@section('css')
+	<link href="{{ Theme::url('plugins/datatable/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+@stop
+@section('javascript')
+	<script src="{{ Theme::url('plugins/datatable/jquery.dataTables.min.js') }}"></script>
+	<script src="{{ Theme::url('plugins/datatable/dataTables.bootstrap4.min.js') }}"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+		    $('#table-1').DataTable();
+		} );
+	</script>
+@stop
