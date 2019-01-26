0f7ee1b745173ad6ffda2be2c949796547b1bf17
Daniel Billing <daniel@infihex.com>
2019-01-26 21:39:28 +0100

    Info

---
+++ resources/views/vobilet-admin/info/edit.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.main')
+@section('title', 'Edit Info - '.$info->name.' - Admin')
+@section('content')
+
+<div class="page-header">
+	<h4 class="page-title">Edit Info:  {{ $info->name }}</h4>
+	<ol class="breadcrumb">
+		<li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+		<li class="breadcrumb-item"><a href="{{ route('admin') }}">Admin</a></li>
+		<li class="breadcrumb-item"><a href="{{ route('admin-info') }}">Info</a></li>
+		<li class="breadcrumb-item active" aria-current="page">Edit Info:  {{ $info->name }}</li>
+	</ol>
+</div>
+
+<div class="row">
+	<div class="col-md-12 col-lg-12">
+
+		<div class="card">
+			<div class="card-body">
+				<form action="{{ route('admin-info-update', $info->id) }}" method="post">
+					<div class="input-group">
+						<input type="text" class="form-control input-lg" name="content" placeholder="Info Value" value="{{ (old('content')) ? old('content') : $info->content }}" />
+						<span class="input-group-append">
+							<button class="btn btn-success" type="submit"><i class="fa fa-save mr-2"></i> Save</button>
+						</span>
+						@if($errors->has('content'))
+							<p class="text-danger">{{ $errors->first('content') }}</p>
+						@endif
+					</div>
+					<input type="hidden" name="_token" value="{{ csrf_token() }}">
+				</form>
+			</div>
+		</div>
+		
+	</div>
+</div>
+
+@stop
---
+++ resources/views/vobilet-admin/info/index.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.main')
+@section('title', 'Info - Admin')
+@section('content')
+
+<div class="page-header">
+	<h4 class="page-title">Info</h4>
+	<ol class="breadcrumb">
+		<li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+		<li class="breadcrumb-item"><a href="{{ route('admin') }}">Admin</a></li>
+		<li class="breadcrumb-item active" aria-current="page">Info</li>
+	</ol>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+
+		<table class="table table-striped table-bordered dataTable no-footer" id="table-1">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Content</th>
+					<th>Actions</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($allinfo as $info)
+					<tr>
+						<td scope="row">{{ $info->name }}</td>
+						<td>{{ $info->content }}</td>
+						<td>
+							<a href="{{ route('admin-info-edit', $info->id) }}" class="btn btn-warning btn-sm"><i class="fas fa-edit mr-2"></i>Edit</a>
+						</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+
+	</div>
+</div>
+@stop
+@section('css')
+	<link href="{{ Theme::url('plugins/datatable/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+	<link href="{{ Theme::url('plugins/datatable/responsive/css/responsive.bootstrap4.css') }}" rel="stylesheet">
+@stop
+@section('javascript')
+	<script src="{{ Theme::url('plugins/datatable/jquery.dataTables.min.js') }}"></script>
+	<script src="{{ Theme::url('plugins/datatable/dataTables.bootstrap4.min.js') }}"></script>
+	<script src="{{ Theme::url('plugins/datatable/responsive/js/datatables.responsive.min.js') }}"></script>
+	<script src="{{ Theme::url('plugins/datatable/responsive/js/responsive.bootstrap4.js') }}"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+		    $('#table-1').DataTable({
+		    	responsive: true
+		    });
+		} );
+	</script>
+@stop
