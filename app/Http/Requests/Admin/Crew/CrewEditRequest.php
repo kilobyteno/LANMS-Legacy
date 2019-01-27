d40fd242e107635f7a63ff0363ccab15cb921699
Daniel Billing <daniel@infihex.com>
2019-01-27 21:43:32 +0100

    PSR-2

--- app/Http/Requests/Admin/Crew/CrewEditRequest.php
+++ app/Http/Requests/Admin/Crew/CrewEditRequest.php
@@ -2,28 +2,28 @@
 
 use LANMS\Http\Requests\Request;
 
-class CrewEditRequest extends Request {
+class CrewEditRequest extends Request
+{
 
-	/**
-	 * Determine if the user is authorized to make this request.
-	 *
-	 * @return bool
-	 */
-	public function authorize()
-	{
-		return true;
-	}
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
 
-	/**
-	 * Get the validation rules that apply to the request.
-	 *
-	 * @return array
-	 */
-	public function rules()
-	{
-		return [
-			'category_id' 	=> 'required|integer',
-		];
-	}
-
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'category_id' => 'required|integer'
+        ];
+    }
 }
