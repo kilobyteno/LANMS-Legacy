43cca22a7ebae02f9d6a1952006bca242debe040
Daniel Billing <daniel@infihex.com>
2019-01-27 14:06:03 +0100

    News + datepicker

---
+++ resources/views/vobilet-admin/news/edit.blade.php
@@ -0,0 +1,195 @@
+@extends('layouts.main')
+@section('title', 'Edit Article: '.$article->title.' - Admin')
+@section('content')
+
+<div class="page-header">
+	<h4 class="page-title">Edit Article: {{ $article->title }}</h4>
+	<ol class="breadcrumb">
+		<li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+		<li class="breadcrumb-item"><a href="{{ route('admin') }}">Admin</a></li>
+		<li class="breadcrumb-item"><a href="{{ route('admin-news') }}">News</a></li>
+		<li class="breadcrumb-item active" aria-current="page">Edit Article #{{ $article->id }}</li>
+	</ol>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+
+		<div class="card">
+			<div class="card-body">
+				<form action="{{ route('admin-news-store') }}" method="post">
+
+					<div class="input-group mb-5">
+						<input type="text" class="form-control input-lg" name="title" autocomplete="off" placeholder="Title" value="{{ (old('title')) ? old('title') : $article->title }}" />
+						<span class="input-group-append">
+							<button class="btn btn-success" type="submit"><i class="fa fa-save mr-2"></i> Save</button>
+						</span>
+						@if($errors->has('title'))
+							<p class="text-danger">{{ $errors->first('title') }}</p>
+						@endif
+					</div>
+
+					<!-- WYSIWYG - Content Editor -->
+					<div class="row">
+						<div class="col-sm-12 @if($errors->has('content')) has-error @endif">
+							<textarea class="content richText-initial" name="content">
+								{{ (old('content')) ? old('content') : $article->content }}
+							</textarea>
+							@if($errors->has('content'))
+								<p class="text-danger">{{ $errors->first('content') }}</p>
+							@endif
+						</div>
+					</div>
+					
+					<br />
+
+					<div class="row">
+					
+						<div class="col-sm-4">
+							
+							<div class="expanel expanel-default @if($errors->has('active')) expanel-danger @endif" data-collapsed="0">
+								<div class="expanel-heading">
+									<div class="expanel-title">Publish Settings</div>
+								</div>
+								<div class="expanel-body">
+									
+									<div class="form-group @if($errors->has('active')) text-danger @endif">
+										<label class="custom-control custom-checkbox">
+											<input type="checkbox" class="custom-control-input" name="active" @if($article->active) checked @endif>
+											<span class="custom-control-label">Show on frontpage</span>
+										</label>
+										<p><small><em>Will be visible on the users dashboard if checked or not</em></small></p>
+										@if($errors->has('active'))
+											<p class="text-danger">{{ $errors->first('active') }}</p>
+										@endif
+									</div>
+									<div class="row">
+										<div class="col-md-6">
+											<p>Publish Date</p>
+											<div class="input-group">
+												<div class="input-group-prepend">
+													<div class="input-group-text">
+														<i class="fa fa-calendar"></i>
+													</div>
+												</div>
+												<input class="form-control datepicker" id="published_at_date" type="text" data-date-format="yyyy-mm-dd" value="{{ (old('published_at_date')) ? old('published_at_date') : date('Y-m-d', strtotime($article->published_at)) }}">
+												@if($errors->has('published_at_date'))
+													<p class="text-danger">{{ $errors->first('published_at_date') }}</p>
+												@endif
+											</div>
+										</div>
+										<div class="col-md-6">
+											<p>Publish Time</p>
+											<div class="input-group">
+												<div class="input-group-prepend">
+													<div class="input-group-text">
+														<i class="fas fa-clock"></i>
+													</div>
+												</div>
+												<input class="form-control ui-timepicker-input @if($errors->has('published_at_time')) is-invalid state-invalid @endif" id="published_at_time" placeholder="Set time" type="text" autocomplete="off" name="published_at_time" value="{{ (old('published_at_time')) ? old('published_at_time') : date('H:i', strtotime($article->published_at)) }}">
+												@if($errors->has('published_at_time'))
+													<p class="text-danger">{{ $errors->first('published_at_time') }}</p>
+												@endif
+											</div>
+										</div>
+									</div>
+											
+								</div>
+							
+							</div>
+							
+						</div>
+						
+						<div class="col-sm-4">
+							
+							<div class="expanel expanel-default @if($errors->has('category_id')) expanel-danger @endif" data-collapsed="0">
+								<div class="expanel-heading">
+									<div class="expanel-title">Category</div>
+								</div>
+								<div class="expanel-body">
+									<select name="category_id" class="select2">
+										@foreach($categories as $category)
+											<option value="{{ $category->id }}" @if($category->id == $article->category->id) selected @endif>{{ $category->name }}</option>
+										@endforeach
+									</select>
+									@if($errors->has('category_id'))
+										<p class="text-danger">{{ $errors->first('category_id') }}</p>
+									@endif
+								</div>
+							</div>
+
+							<div class="expanel expanel-default @if($errors->has('slug')) expanel-danger @endif" data-collapsed="0">
+								<div class="expanel-heading">
+									<div class="expanel-title">Slug <small class="text-muted">*Optional</small></div>
+								</div>
+								<div class="expanel-body">
+									<div class="input-group">
+										<div class="input-group-prepend">
+											<div class="input-group-text">
+												.../news/
+											</div>
+										</div>
+										<input type="text" class="form-control" value="{{ (old('slug')) ? old('slug') : $article->slug }}" name="slug">
+									</div>
+									@if($errors->has('slug'))
+										<p class="text-danger">{{ $errors->first('slug') }}</p>
+									@endif
+								</div>
+							</div>
+							
+						</div>
+						
+					</div>
+
+					<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+				</form>
+			</div>
+		</div>
+
+	</div>
+</div>
+
+@stop
+@section('css')
+	<link href="{{ Theme::url('plugins/time-picker/jquery.timepicker.css') }}" rel="stylesheet">
+	<link href="{{ Theme::url('js/vendors/bootstrap-datepicker3.standalone.css') }} rel="stylesheet">
+	<link href="{{ Theme::url('plugins/wysiwyag/richtext.min.css') }}" rel="stylesheet">
+@stop
+@section('javascript')
+
+	<script src="{{ Theme::url('js/vendors/bootstrap-datepicker.js') }}"></script>
+
+	<!-- Time -->
+	<script src="{{ Theme::url('plugins/time-picker/jquery.timepicker.js') }}"></script>
+
+	<!-- maskedinput -->
+	<script src="{{ Theme::url('plugins/input-mask/jquery.maskedinput.js') }}"></script>
+
+	<!-- Activations -->
+	<script type="text/javascript">
+		$(function(){
+		    $('#published_at_time').timepicker({
+		    	'scrollDefault': 'now',
+		    	'timeFormat': 'H:i',
+		    	'step': 10
+		    });
+		    $('.select2').select2();
+		    $('.datepicker').datepicker({
+		    	format: 'yyyy-mm-dd',
+		    	todayBtn: "linked",
+			    calendarWeeks: true,
+			    autoclose: true,
+			    todayHighlight: true
+		    });
+		});
+	</script>
+
+	<script src="{{ Theme::url('plugins/wysiwyag/jquery.richtext.js') }}"></script>
+	<script type="text/javascript">
+		$(function(e) {
+			$('.content').richText();
+			$('.richText-help').remove(); // Remove useless help icon
+		});
+	</script>
+@stop
---
+++ resources/views/vobilet-admin/news/index.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.main')
+@section('title', 'News - Admin')
+@section('content')
+
+<div class="page-header">
+	<h4 class="page-title">News @if(Sentinel::hasAccess('admin.news.create'))<a class="btn btn-sm btn-success ml-2" href="{{ route('admin-news-create') }}"><i class="fa fa-plus mr-2"></i> Create</a>@endif</h4>
+	<ol class="breadcrumb">
+		<li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+		<li class="breadcrumb-item"><a href="{{ route('admin') }}">Admin</a></li>
+		<li class="breadcrumb-item active" aria-current="page">News</li>
+	</ol>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+		
+		<table class="table table-striped table-bordered dataTable no-footer" id="table-1">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Title</th>
+					<th>Category</th>
+					<th>Published at</th>
+					<th>Created by</th>
+					<th>Edited at</th>
+					<th>Edited by</th>
+					<th>Actions</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($news as $article)
+					<tr>
+						<th scope="row">{{ $article->id }}</th>
+						<td>{{ $article->title }}</td>
+						<td><a href="{{ route('admin-news-category') }}"><div class="badge badge-info"><i class="fa fa-tag"></i> {{ $article->category->name }}</div></a></td>
+						<td>{{ ucfirst(\Carbon::parse($article->published_at)->isoFormat('LLL')) }}</td>
+						<td><a href="{{ URL::route('user-profile', $article->author->username) }}">{{ User::getFullnameByID($article->author->id) }}</a></td>
+						<td>{{ ucfirst(\Carbon::parse($article->edited_at)->isoFormat('LLL')) }}</td>
+						<td><a href="{{ URL::route('user-profile', $article->editor->username) }}">{{ User::getFullnameByID($article->editor->id) }}</a></td>
+						<td>
+							<a href="{{ route('news-show', $article->slug) }}" class="btn btn-info btn-sm"><i class="fas fa-eye mr-2"></i>View</a>
+							<a href="{{ route('admin-news-edit', $article->id) }}" class="btn btn-warning btn-sm"><i class="fas fa-edit mr-2"></i>Edit</a>
+							@if(Sentinel::hasAccess('admin.news.destroy'))
+								<a href="javascript:;" onclick="jQuery('#news-destroy-{{ $article->id }}').modal('show', {backdrop: 'static'});" class="btn btn-danger btn-sm"><i class="fas fa-trash mr-2"></i>Delete</a>
+							@endif
+						</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+
+	</div>
+</div>
+
+@foreach($news as $article)
+	<div class="modal fade" id="news-destroy-{{ $article->id }}" data-backdrop="static">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title"><strong>Delete Article:</strong> #{{ $article->id }} - {{ $article->title }}</h4>
+				</div>
+				<div class="modal-body">
+					<h4 class="text-danger text-center"><strong>Are you sure you want to delete this article?</strong></h4>
+				</div>
+				<div class="modal-footer">
+					<a href="{{ route('admin-news-destroy', $article->id) }}" class="btn btn-danger">Yes, I want to delete it.</a>
+					<button type="button" class="btn btn-success" data-dismiss="modal">No, take me away!</button>
+				</div>
+			</div>
+		</div>
+	</div>
+@endforeach
+
+@stop
+
+@section('css')
+	<link href="{{ Theme::url('plugins/datatable/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+@stop
+@section('javascript')
+	<script src="{{ Theme::url('plugins/datatable/jquery.dataTables.min.js') }}"></script>
+	<script src="{{ Theme::url('plugins/datatable/dataTables.bootstrap4.min.js') }}"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+		    $('#table-1').DataTable({
+		        "order": [[ 0, "desc" ]]
+		    });
+		});
+	</script>
+@stop
