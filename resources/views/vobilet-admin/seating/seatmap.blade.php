9469b88068d03f2e5afd6b7adbb31f036d3bef2d
Daniel Billing <daniel@infihex.com>
2019-01-28 20:56:54 +0100

    Improved admin seatmap

--- resources/views/vobilet-admin/seating/seatmap.blade.php
+++ resources/views/vobilet-admin/seating/seatmap.blade.php
@@ -12,10 +12,10 @@
 					@endif
 					@foreach($row->seats as $seat)
 					
-						<li class="seat @if($seat->reservationsThisYear()->count() > 0) @if($seat->reservationsThisYear()->first()->status->id == 1) seat-reserved @elseif($seat->reservationsThisYear()->first()->status->id == 2) seat-tempreserved @endif @if(Sentinel::getUser()->id == $seat->reservationsThisYear()->first()->reservedfor->id and $seat->reservationsThisYear()->first()->status->id == 1) seat-yours @endif  @endif @if(Request::segment(3) == $seat->slug) active @endif ">
+						<li class="seat @if($seat->reservationsThisYear()->count() > 0) @if($seat->reservationsThisYear()->first()->status->id == 1) seat-reserved @elseif($seat->reservationsThisYear()->first()->status->id == 2) seat-tempreserved @endif @if(Sentinel::getUser()->id == $seat->reservationsThisYear()->first()->reservedfor->id and $seat->reservationsThisYear()->first()->status->id == 1) seat-yours @endif  @endif @if(Request::segment(4) == $seat->slug) active @endif ">
 							<p>
 								@if($seat->reservationsThisYear()->count() == 0)
-									<a href="{{ route('seating-show', $seat->slug) }}" data-toggle="tooltip" title="{{ trans('seating.map.available') }}">{{ $seat->name }}</a>
+									<a href="{{ route('admin-seating-reservation-show', $seat->slug) }}" data-toggle="tooltip" title="{{ trans('seating.map.available') }}">{{ $seat->name }}</a>
 								@elseif(Sentinel::getUser()->id == $seat->reservationsThisYear()->first()->reservedfor->id and $seat->reservationsThisYear()->first()->status->id == 1)
 									<a href="{{ route('admin-seating-reservation-edit', $seat->reservationsThisYear()->first()->id) }}" data-toggle="tooltip" title="{{ trans('seating.map.you') }}">{{ $seat->name }}</a>
 								@elseif($seat->reservationsThisYear()->first()->status->id == 1)
