2c4911effb1b6ed9db56d57385f02399f207b872
Daniel Billing <daniel@infihex.com>
2019-02-03 12:06:27 +0100

    Atendee checkin improvements

--- resources/views/vobilet-admin/seating/checkin/index.blade.php
+++ resources/views/vobilet-admin/seating/checkin/index.blade.php
@@ -18,17 +18,15 @@
 		<div class="card">
 			<div class="card-body">
 				<form action="{{ route('admin-seating-checkin-check') }}" method="post">
-					<div class="row">
-						<div class="col-sm-10 @if($errors->has('barcode')) has-error @endif">
-							<input type="text" class="form-control input-lg" name="barcode" placeholder="Barcode (12345678910)" value="{{ (old('barcode')) ? old('barcode') : '' }}" autocomplete="off" autofocus />
-							@if($errors->has('barcode'))
-								<p class="text-danger">{{ $errors->first('barcode') }}</p>
-							@endif
-						</div>
-						<div class="col-sm-2">
+					<div class="input-group">
+						<input type="text" class="form-control input-lg" name="barcode" placeholder="Barcode (12345678910)" value="{{ (old('barcode')) ? old('barcode') : '' }}" autocomplete="off" autofocus />
+						<div class="input-group-append">
 							<button type="submit" class="btn btn-success btn-block btn-icon"><i class="fas fa-search mr-2"></i>Find Ticket</button>
 						</div>
 					</div>
+					@if($errors->has('barcode'))
+						<p class="text-danger">{{ $errors->first('barcode') }}</p>
+					@endif
 					<input type="hidden" name="_token" value="{{ csrf_token() }}">
 				</form>
 			</div>
