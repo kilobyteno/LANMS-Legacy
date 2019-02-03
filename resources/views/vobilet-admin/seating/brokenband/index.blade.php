9e2ea53cbc43e8f065f003caad1c7f9a89941466
Daniel Billing <daniel@infihex.com>
2019-02-03 12:01:24 +0100

    Design improvements for broken bands

--- resources/views/vobilet-admin/seating/brokenband/index.blade.php
+++ resources/views/vobilet-admin/seating/brokenband/index.blade.php
@@ -17,21 +17,22 @@
 
 		<div class="row">
 			<div class="col-md-6">
-				<form action="{{ route('admin-seating-brokenband-check') }}" method="post">
-					<div class="row">
-						<div class="col-sm-10 @if($errors->has('band_id')) has-error @endif">
-							<input type="text" class="form-control input-lg" name="band_id" placeholder="Band ID" value="{{ (old('band_id')) ? old('band_id') : '' }}" autocomplete="off" autofocus />
+				<div class="card">
+					<div class="card-body">
+						<form action="{{ route('admin-seating-brokenband-check') }}" method="post">
+							<div class="input-group">
+								<input type="text" class="form-control input-lg" name="band_id" placeholder="Band ID" value="{{ (old('band_id')) ? old('band_id') : '' }}" autocomplete="off" autofocus />
+								<span class="input-group-append">
+									<button type="submit" class="btn btn-success btn-block"><i class="fas fa-search mr-2"></i>Find Band</button>
+								</span>
+							</div>
 							@if($errors->has('band_id'))
 								<p class="text-danger">{{ $errors->first('band_id') }}</p>
 							@endif
-						</div>
-						<div class="col-sm-2">
-							<button type="submit" class="btn btn-success btn-block"><i class="fas fa-search mr-2"></i>Find Band</button>
-						</div>
+							<input type="hidden" name="_token" value="{{ csrf_token() }}">
+						</form>
 					</div>
-					<input type="hidden" name="_token" value="{{ csrf_token() }}">
-				</form>
-				<hr>
+				</div>
 				<h1 class="text-center">{{ BrokenBand::thisYear()->count() }}<br><small>Broken bands this year</small></h1>
 				<hr>
 				<table class="table">
