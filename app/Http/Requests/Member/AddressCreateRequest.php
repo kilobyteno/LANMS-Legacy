63c312e5a05649350990a02dac665a21471d7917
Daniel Billing <daniel@infihex.com>
2019-03-07 22:01:38 +0100

    LANMS-293

--- app/Http/Requests/Member/AddressCreateRequest.php
+++ app/Http/Requests/Member/AddressCreateRequest.php
@@ -2,33 +2,33 @@
 
 use LANMS\Http\Requests\Request;
 
-class AddressCreateRequest extends Request {
+class AddressCreateRequest extends Request
+{
 
-	/**
-	 * Determine if the user is authorized to make this request.
-	 *
-	 * @return bool
-	 */
-	public function authorize()
-	{
-		return true;
-	}
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
 
-	/**
-	 * Get the validation rules that apply to the request.
-	 *
-	 * @return array
-	 */
-	public function rules()
-	{
-		return [
-			'address1' 		=> 'required|regex:/^[A-Za-z \Wæøå]+$/|max:100',
-			'address2' 		=> 'alpha_num',
-			'postalcode' 	=> 'required|alpha_dash|min:4',
-			'city' 			=> 'required|regex:/^[A-Za-z \Wæøå]+$/',
-			'county' 		=> 'required|regex:/^[A-Za-z \Wæøå]+$/',
-			'country' 		=> 'required|alpha',
-		];
-	}
-
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'address1' 		=> 'required|regex:/^[A-Za-z \Wæøå]+$/|max:100',
+            'address2' 		=> 'required|alpha_num',
+            'postalcode' 	=> 'required|alpha_dash|min:4',
+            'city' 			=> 'required|regex:/^[A-Za-z \Wæøå]+$/',
+            'county' 		=> 'required|regex:/^[A-Za-z \Wæøå]+$/',
+            'country' 		=> 'required|alpha',
+        ];
+    }
 }
