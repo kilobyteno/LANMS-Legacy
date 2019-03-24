74abc45f999097f7d3df6bfccfa6604d551c9c5b
Daniel Billing <daniel@infihex.com>
2019-03-24 18:04:57 +0100

    LANMS-211

--- resources/views/vobilet-admin/layouts/main.blade.php
+++ resources/views/vobilet-admin/layouts/main.blade.php
@@ -194,6 +194,14 @@
 								<div class="col-md-12 col-sm-12 mt-3 mt-lg-0 text-center">
 									&copy; {{ Setting::get('WEB_COPYRIGHT') }} &middot; <i class="fa fa-coffee"></i> {{ round((microtime(true) - LARAVEL_START), 3) }}s</small>
 									<br>
+									<div class="dropup btn-group mt-2 mb-2">
+										<button class="btn btn-default btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="true"><i class="fas fa-language"></i> {{ mb_strtoupper(App::getLocale()) }}<span class="caret"></span></button>
+										<ul class="dropdown-menu" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 38px, 0px); top: 0px; left: 0px; will-change: transform;">
+											@foreach(array_flip(config('app.locales')) as $lang)
+												<li><a href="{{ route('locale', $lang) }}">{{ trans('language.'.$lang) }}</a></li>
+											@endforeach
+										</ul>
+									</div>
 									<p class="mt-2"><a href="http://lanms.xyz/" target="_blank">{{ Setting::get('APP_NAME') }}</a> <a href="{{ Setting::get('APP_URL') }}">{{ Setting::get('APP_VERSION') . ' ' . Setting::get('APP_VERSION_TYPE') }}</a> {{ trans('global.by') }} <a href="https://infihex.com/" target="_blank">Infihex</a></p>
 									@if(Setting::get('APP_LICENSE_STATUS') == "Invalid")<b class="text-danger">Unlicensed version of this software!</b>@elseif(Setting::get('APP_LICENSE_STATUS') == "Expired")<b class="text-danger">License has expired for this software!</b>@endif
 									@if(Config::get('app.debug'))
