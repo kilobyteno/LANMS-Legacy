78fcc2af7ad4d1bb56311d481cd7a25c03084229
Daniel Billing <daniel@infihex.com>
2019-03-31 20:40:04 +0200

    LANMS-240 and PSR-2 parse

--- app/Page.php
+++ app/Page.php
@@ -2,31 +2,43 @@
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
 
-class Page extends Model {
+class Page extends Model
+{
+    use SoftDeletes, LogsActivity;
 
-	use SoftDeletes;
+    protected $dates = ['deleted_at'];
+    protected $table = 'pages';
 
-	protected $dates = ['deleted_at'];
-	protected $table = 'pages';
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+        'active',
+    ];
 
-	protected $fillable = [
-		'title',
-		'slug',
-		'content',
-		'active'
-	];
+    protected static $logName = 'page';
+    protected static $logOnlyDirty = true;
+    protected static $logAttributes = [
+        'title',
+        'slug',
+        'content',
+        'active',
+    ];
 
-	function author() {
-		return $this->hasOne('User', 'id', 'author_id');
-	}
+    public function author()
+    {
+        return $this->hasOne('User', 'id', 'author_id');
+    }
 
-	function editor() {
-		return $this->hasOne('User', 'id', 'editor_id');
-	}
+    public function editor()
+    {
+        return $this->hasOne('User', 'id', 'editor_id');
+    }
 
-	function scopeForMenu($query) {
-		return $this->where('active', '=', 1)->where('showinmenu', '=', 1)->get();
-	}
-
+    public function scopeForMenu($query)
+    {
+        return $this->where('active', '=', 1)->where('showinmenu', '=', 1)->get();
+    }
 }
--- app/Sponsor.php
+++ app/Sponsor.php
@@ -4,10 +4,11 @@ namespace LANMS;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Sponsor extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, LogsActivity;
 
     protected $dates = ['deleted_at'];
     protected $fillable = [
@@ -20,6 +21,15 @@ class Sponsor extends Model
         'editor_id',
     ];
     protected $table = 'sponsors';
+
+    protected static $logName = 'sponsor';
+    protected static $logOnlyDirty = true;
+    protected static $logAttributes = [
+        'name',
+        'url',
+        'description',
+        'sort_order',
+    ];
 
     public function scopeOrdered($query)
     {
