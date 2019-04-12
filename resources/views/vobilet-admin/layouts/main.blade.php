4b05ec3a37c22567d34637f5f5fb838a40e256ca
Daniel Billing <daniel@infihex.com>
2019-04-12 13:54:07 +0200

    Changed to stripe logo, maybe keeping it LANMS-23

--- resources/views/vobilet-admin/layouts/main.blade.php
+++ resources/views/vobilet-admin/layouts/main.blade.php
@@ -112,7 +112,7 @@
 							<a class="side-menu__item @if(Request::is('admin')){{'active'}} @endif" href="{{ route('admin') }}"><i class="side-menu__icon fa fa-tachometer-alt"></i><span class="side-menu__label">Dashboard</span></a>
 						</li>
 						<li class="slide @if(Request::is('admin/billing*')){{'is-expanded'}} @endif">
-							<a class="side-menu__item @if(Request::is('admin/billing*')){{'active'}} @endif" data-toggle="slide" href="#"><i class="side-menu__icon fas fa-money-bill-alt"></i><span class="side-menu__label">Billing</span><i class="angle fa fa-angle-right"></i></a>
+							<a class="side-menu__item @if(Request::is('admin/billing*')){{'active'}} @endif" data-toggle="slide" href="#"><i class="side-menu__icon fab fa-stripe"></i><span class="side-menu__label">Billing</span><i class="angle fa fa-angle-right"></i></a>
 							<ul class="slide-menu">
 								<li><a class="slide-item @if(Request::is('admin/billing/invoice*')){{'active'}} @endif" href="{{ route('admin-billing-invoice') }}"><i class="fas fa-file-invoice mr-1"></i> Invoice</a></li>
 							</ul>
