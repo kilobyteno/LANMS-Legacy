209e1729bc4137aa9e9aefbd818b1094d3581a32
Daniel Billing <daniel@infihex.com>
2019-09-15 11:30:57 +0200

    WIP edit view LANMS-272

---
+++ resources/views/vobilet-admin/user/role/edit.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.main')
+@section('title', 'Edit Role - Admin')
+@section('content')
+
+<div class="page-header">
+	<h4 class="page-title">Edit Role</h4>
+	<ol class="breadcrumb">
+		<li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+		<li class="breadcrumb-item"><a href="{{ route('admin') }}">Admin</a></li>
+		<li class="breadcrumb-item"><a href="{{ route('admin-roles') }}">Roles</a></li>
+		<li class="breadcrumb-item active" aria-current="page">Edit Role</li>
+	</ol>
+</div>
+
+<div class="row">
+	<div class="col-md-12">
+
+		<div class="card">
+			<div class="card-body">
+				<form action="{{ route('admin-role-update', $role->slug) }}" method="post">
+
+					<div class="input-group mb-5">
+						<div class="input-group-prepend">
+							<div class="input-group-text">Name:</div>
+						</div>
+						<input type="text" class="form-control input-lg" name="name" autocomplete="off" placeholder="Alphabet Inc." value="{{ (old('name')) ? old('name') : $role->name }}" />
+						<span class="input-group-append">
+							<button class="btn btn-success" type="submit"><i class="fa fa-save mr-2"></i> Save</button>
+						</span>
+						@if($errors->has('name'))
+							<p class="text-danger">{{ $errors->first('name') }}</p>
+						@endif
+					</div>
+
+					<div class="row">
+						
+					</div>
+
+					<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+				</form>
+			</div>
+		</div>
+
+	</div>
+</div>
+
+@stop
