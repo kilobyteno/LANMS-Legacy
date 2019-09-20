7e1d929a40212009efc9fed85487ca86d628073d
Daniel Billing <daniel@infihex.com>
2019-09-20 13:53:38 +0200

    Finished model for LANMS-333

--- app/NewsCategory.php
+++ app/NewsCategory.php
@@ -3,20 +3,30 @@
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class NewsCategory extends Model {
+class NewsCategory extends Model
+{
+    use SoftDeletes;
 
-	use SoftDeletes;
+    protected $dates = ['deleted_at'];
+    protected $fillable = [
+        'name',
+        'slug',
+        'creator_id',
+    ];
+    protected $table = 'news_categories';
 
-	protected $dates = ['deleted_at'];
-	protected $fillable = [
-		'name',
-		'slug',
-		'creator_id',
-	];
-	protected $table = 'news_categories';
+    public function articles()
+    {
+        return $this->hasMany('News', 'category_id');
+    }
 
-	public function categories() {
-		return $this->hasMany('News', 'category_id');
-	}
+    public function author()
+    {
+        return $this->hasOne('User', 'id', 'author_id')->withTrashed();
+    }
 
+    public function editor()
+    {
+        return $this->hasOne('User', 'id', 'editor_id')->withTrashed();
+    }
 }
