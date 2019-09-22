c6d89af3ab514bec2f96e646bc2cbb932a15da04
Daniel Billing <daniel@infihex.com>
2019-09-22 20:11:16 +0200

    Resolve conflict

--- app/NewsCategory.php
+++ app/NewsCategory.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class NewsCategory extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, LogsActivity;
 
     protected $dates = ['deleted_at'];
     protected $fillable = [
@@ -14,6 +14,13 @@ class NewsCategory extends Model
         'creator_id',
     ];
     protected $table = 'news_categories';
+
+    protected static $logName = 'news_category';
+    protected static $logOnlyDirty = true;
+    protected static $logAttributes = [
+        'name',
+        'slug',
+    ];
 
     public function articles()
     {
