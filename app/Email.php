807e95ecc05e2079ae23d5f164fe1978bece67b0
Daniel Billing <daniel@infihex.com>
2019-09-22 19:41:13 +0200

    Email model, seeder and migrations

---
+++ app/Email.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace LANMS;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Email extends Model
+{
+    protected $fillable = [
+        'content',
+        'author_id',
+    ];
+    protected $table = 'emails';
+
+    public function author()
+    {
+        return $this->hasOne('User', 'id', 'author_id');
+    }
+
+    /**
+     * Get the users for the email.
+     */
+    public function users()
+    {
+        return $this->belongsToMany('LANMS\User');
+    }
+}
---
+++ database/migrations/2019_09_22_190057_create_email_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emails', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            
+            $table->text('content');
+
+            $table->integer('author_id')->default(0); //who created it?
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emails');
+    }
+}
