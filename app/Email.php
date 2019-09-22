1741228d9161471b7a977818e18f74fcbce4026d
Daniel Billing <daniel@infihex.com>
2019-09-22 20:07:35 +0200

    Added activity logging to email

--- app/Email.php
+++ app/Email.php
@@ -3,15 +3,27 @@
 namespace LANMS;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Email extends Model
 {
+
+    use LogsActivity;
+
     protected $fillable = [
         'subject',
         'content',
         'author_id',
     ];
     protected $table = 'emails';
+
+    protected static $logName = 'email';
+    protected static $logOnlyDirty = true;
+    protected static $logAttributes = [
+        'subject',
+        'content',
+        'author_id',
+    ];
 
     public function author()
     {
