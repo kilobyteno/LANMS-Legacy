f23e844ca8709e5147670fc68165880a89adf6f3
Daniel Billing <daniel@infihex.com>
2019-10-02 21:59:59 +0200

    Added command for checking for notifications LANMS-358

---
+++ app/Console/Commands/InvoiceUnpaid.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace LANMS\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Notification;
+use LANMS\Notifications\InvoiceUnPaid as InvUnpaid;
+use LANMS\User;
+use Illuminate\Support\Facades\DB;
+
+class InvoiceUnpaid extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'lanms:invoiceunpaid';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Checks for unpaid invoices for a user and creates a notification.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        foreach (User::active() as $user) {
+            if ($user->stripecustomer) {
+                $stripe_customer_code = $user->stripecustomer->cus;
+                $invoices = \Stripe::invoices()->all(array('customer' => $stripe_customer_code, 'limit' => 100));
+                $invoices = $invoices['data'];
+                foreach ($invoices as $invoice) {
+                    if ($invoice['paid'] == false) {
+                        $data = [
+                            'amount_due' => $invoice['amount_due'],
+                            'due_date' => $invoice['due_date'],
+                            'currency' => $invoice['currency'],
+                            'url' => route('account-billing-invoice-view', $invoice['id']),
+                        ];
+                        $db_data = collect($data)->toJson();
+                        // Check if there is a notification already
+                        $notification = DB::table('notifications')->where('data', $db_data)->where('read_at', null)->first();
+                        if (!$notification) {
+                            Notification::send($user, new InvUnpaid($invoice));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
