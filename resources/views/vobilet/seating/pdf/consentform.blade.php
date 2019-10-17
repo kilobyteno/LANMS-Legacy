ac6d3ff55336d747f6fa0b8b88f29323b0b03a13
Daniel Billing <daniel@infihex.com>
2019-10-17 20:07:46 +0200

    Minor pdf improvements

--- resources/views/vobilet/seating/pdf/consentform.blade.php
+++ resources/views/vobilet/seating/pdf/consentform.blade.php
@@ -49,10 +49,10 @@
 		</div>
 		<div class="row">
 			<div class="col-md-12">
-				<p>{!! trans('pdf.consentform.moreinfo', ['url'=>Setting::get('WEB_PROTOCOL').'://'.Setting::get('WEB_DOMAIN').'/tilforeldre']) !!}</p>
+				<p>{!! trans('pdf.consentform.moreinfo', ['url'=>Setting::get('WEB_PROTOCOL').'://'.Setting::get('WEB_DOMAIN')]) !!}</p>
 				<hr>
 				<h2>{!! trans('pdf.consentform.permission', ['event'=>Setting::get('WEB_NAME').' '.Setting::get('SEATING_YEAR')]) !!}</h2>
-				<p>{!! trans('pdf.consentform.permissiondesc', ['event'=>Setting::get('WEB_NAME').' '.Setting::get('SEATING_YEAR'), 'name' => ($user ? LANMS\User::getFullnameByID($user->id) : '______________________________________________________'), 'birthdate' => ($user ? $user->birthdate : '__________________'), 'location'=>LANMS\Info::getContent('where').' '.LANMS\Info::getContent('when')]) !!}</p>
+				<p>{!! trans('pdf.consentform.permissiondesc', ['event'=>Setting::get('WEB_NAME').' '.Setting::get('SEATING_YEAR'), 'name' => ($user ? LANMS\User::getFullnameByID($user->id) : '______________________________________________________'), 'birthdate' => ($user ? $user->birthdate : '__________________'), 'location'=>LANMS\Info::getContent('where').', '.LANMS\Info::getContent('when')]) !!}</p>
 				<p>{!! trans('pdf.consentform.myrelationship') !!}<br>______________________________________________________________________</p>
 				<p>{!! trans('pdf.consentform.contact') !!}:</p>
 				<p>{{ trans('global.fullname') }}: ____________________________________________________________________<br>{{ trans('global.phone') }}: _________________________________<br>{{ trans('global.email') }}: _________________________________</p>
