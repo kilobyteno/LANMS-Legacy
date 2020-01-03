7c451e48512c2c632347e62172d04ae170355651
Daniel Billing <daniel@infihex.com>
2020-01-03 21:51:00 +0100

    A few changes to the seeder LANMS-398

--- database/seeds/TicketTypeTableSeeder.php
+++ database/seeds/TicketTypeTableSeeder.php
@@ -13,7 +13,7 @@ class TicketTypeTableSeeder extends Seeder
             'slug' => Str::slug('Deltaker'),
             'description' => 'Torsdag kl16 til søndag kl12, Selvvalgt plass, 56stk plasser',
             'price' => 300,
-            'color' => '4bcffa',
+            'color' => 'ff5e57',
             'default' => true,
         ]);
 
@@ -22,7 +22,7 @@ class TicketTypeTableSeeder extends Seeder
             'slug' => Str::slug('2-dagers'),
             'description' => 'Tors-fre (16-20) eller lør-søn (08-12), Egen plass på rad K, Kun 8stk',
             'price' => 200,
-            'color' => '575fcf',
+            'color' => '3c40c6',
         ]);
 
         TicketType::create([
@@ -30,7 +30,7 @@ class TicketTypeTableSeeder extends Seeder
             'slug' => Str::slug('Dagsbillett'),
             'description' => '10-22 (12 timer), Tilfeldig ledig plass, Veldig begrenset',
             'price' => 100,
-            'color' => 'ef5777',
+            'color' => '575fcf',
         ]);
 
         TicketType::create([
@@ -38,7 +38,15 @@ class TicketTypeTableSeeder extends Seeder
             'slug' => Str::slug('Premium'),
             'description' => 'Torsdag kl.12 til søndag kl.12, Selvalgt plass på premium radene B og C, Kun 10stk, Premium goder: Tidligere innslipp (4 timer), større plasser (ca 120cm), 1stk frokost, 1stk Baguett, 1stk brus, 1stk pizza, medlemskap i Downlink DG som gir deg rabatt på t-skjorte og hettegenser*, og mulig flere goder.',
             'price' => 500,
-            'color' => '0fbcf9',
+            'color' => 'ff3f34',
+        ]);
+
+        TicketType::create([
+            'name' => 'Besøk',
+            'slug' => Str::slug('Besøk'),
+            'description' => 'Kun mellom 07 og 23, besøk utenfor dette tidsrommet koster 50kr',
+            'price' => 0,
+            'color' => '4bcffa',
         ]);
     }
 }
