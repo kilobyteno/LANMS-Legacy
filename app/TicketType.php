10bdc865584195d875aa41790725d4a8b92fbcca
Daniel Billing <daniel@infihex.com>
2020-01-03 23:50:42 +0100

    Added fillable and logging

--- app/TicketType.php
+++ app/TicketType.php
@@ -3,20 +3,36 @@
 namespace LANMS;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class TicketType extends Model
 {
+    use SoftDeletes, LogsActivity;
+
     protected $dates = ['deleted_at'];
     protected $table = 'ticket_types';
 
     protected $fillable = [
         'title',
+        'slug',
+        'description',
+        'price',
+        'default',
+        'active',
+        'editor_id',
+        'author_id',
     ];
 
-    protected static $logName = 'ticket_type';
+    protected static $logName = 'tickettype';
     protected static $logOnlyDirty = true;
     protected static $logAttributes = [
         'title',
+        'slug',
+        'description',
+        'price',
+        'default',
+        'active',
     ];
 
     public function author()
