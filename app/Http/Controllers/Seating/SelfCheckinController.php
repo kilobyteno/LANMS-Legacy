ad8f947ad32abe24e9a1a82d09d975b3dc3886bb
Daniel Billing <daniel@infihex.com>
2020-02-22 20:51:57 +0100

    LANMS-328

---
+++ app/Http/Controllers/Seating/SelfCheckinController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace LANMS\Http\Controllers\Seating;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use LANMS\Http\Controllers\Controller;
+
+class SelfCheckinController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('seating.checkin.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $seatticket = SeatTicket::where('code', $id)->first();
+        if (!$seatticket) {
+            return Redirect::route('seating')->with('messagetype', 'warning')
+                                ->with('message', trans('seating.checkin.alert.notfound'));
+        }
+        dd($seatticket);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
