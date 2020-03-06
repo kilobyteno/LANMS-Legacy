a27302c19e7da75a98bac62edec297bb5fa3827f
Daniel Billing <daniel@infihex.com>
2020-03-06 15:16:52 +0100

    LANMS-457

--- resources/views/vobilet-admin/compo/index.blade.php
+++ resources/views/vobilet-admin/compo/index.blade.php
@@ -35,6 +35,7 @@
 								<td>{{ \Carbon::parse($compo->last_sign_up_at)->toDateTimeString() }}</td>
 								<td>{{ \Carbon::parse($compo->end_at)->toDateTimeString() }}</td>
 								<td>
+									<a href="{{ route('compo-show', $compo->slug) }}" class="btn btn-info btn-sm"><i class="fas fa-eye mr-2"></i>View</a>
 									<a href="{{ route('admin-compo-edit', $compo->id) }}" class="btn btn-warning btn-sm"><i class="fa fa-edit mr-2"></i>Edit</a>
 									@if(Sentinel::hasAccess('admin.compo.destroy'))
 										<a href="javascript:;" onclick="jQuery('#compo-destroy-{{ $compo->id }}').modal('show', {backdrop: 'static'});" class="btn btn-danger btn-sm"><i class="fa fa-trash mr-2"></i>Delete</a>
