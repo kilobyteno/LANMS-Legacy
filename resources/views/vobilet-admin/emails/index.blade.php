e6d89d9ebcd88427c1c13be596083ed27bad4009
Daniel Billing <daniel@infihex.com>
2020-09-24 21:58:54 +0200

    LANMS-483

--- resources/views/vobilet-admin/emails/index.blade.php
+++ resources/views/vobilet-admin/emails/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="page-header">
-	<h4 class="page-title">Emails @if(Sentinel::hasAccess('admin.emails.create'))<a class="btn btn-sm btn-success ml-2" href="{{ route('admin-emails-create') }}"><i class="fa fa-plus mr-2"></i> Create</a>@endif</h4>
+	<h4 class="page-title">Emails @if(Sentinel::hasAccess('admin.emails.create'))<a class="btn btn-sm btn-success ml-2" href="{{ route('admin-emails-create') }}"><i class="fa fa-plus mr-2"></i> Create</a><a class="btn btn-sm btn-success ml-2" href="{{ route('admin-emails-test') }}" data-toggle="tooltip" data-placement="bottom" title="This will send a test email to you. It will not populate the list below."><i class="far fa-paper-plane"></i> Test</a>@endif</h4>
 	<ol class="breadcrumb">
 		<li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
 		<li class="breadcrumb-item"><a href="{{ route('admin') }}">Admin</a></li>
