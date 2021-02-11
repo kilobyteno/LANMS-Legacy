cb3066e4e33df49b111f31d9d10664a5ead69ed4
Daniel S. Billing <daniel@infihex.com>
2021-02-11 22:58:22 +0100

    L8

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -1,31 +1,32 @@
-<?php namespace LANMS\Providers;
+<?php
 
-use Illuminate\Support\Facades\Event;
+namespace LANMS\Providers;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Event;
 
-class EventServiceProvider extends ServiceProvider {
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        Registered::class => [
+            SendEmailVerificationNotification::class,
+        ],
+    ];
 
-	/**
-	 * The event handler mappings for the application.
-	 *
-	 * @var array
-	 */
-	protected $listen = [
-		'LANMS\Events\Event' => [
-			'LANMS\Listeners\EventListener',
-		],
-	];
-
-	/**
-	 * Register any other events for your application.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		parent::boot();
-
-		//
-	}
-
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
 }
