b2ad4d85d2f7b86b29aec9b9d18ab5c9c1df9ac9
Daniel S. Billing <daniel.billing@intility.no>
2022-01-17 11:07:48 +0100

    Update 2018_10_07_211058_update_users_table_with_about.php

--- database/migrations/2018_10_07_211058_update_users_table_with_about.php
+++ database/migrations/2018_10_07_211058_update_users_table_with_about.php
@@ -14,7 +14,7 @@ class UpdateUsersTableWithAbout extends Migration
     public function up()
     {
         Schema::table('users', function ($table) {
-            $table->string('about');
+            $table->string('about')->nullable();
         });
     }
 
